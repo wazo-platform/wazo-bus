1d780bb59f59684a0935ba2bf11be72545c38c9a
Etienne Lessard <elessard@avencall.com>
2013-11-04 14:55:29 -0500

    separate __future__ import from others
    Module is not valid when __future__ is not on top.

--- xivo-bus/xivo_bus/resources/device/event.py
+++ xivo-bus/xivo_bus/resources/device/event.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from __future__ import unicode_literals
+
 from xivo_bus.resources.common.event import ResourceConfigEvent
 
 
