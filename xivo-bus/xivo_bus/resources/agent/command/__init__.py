d2ef1aa9c3fcf877c3d3c37b8bbd1218bbbbff07
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2014-01-03 09:23:32 -0500

    update copyright

--- xivo-bus/xivo_bus/resources/agent/command/__init__.py
+++ xivo-bus/xivo_bus/resources/agent/command/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2012-2013 Avencall
+# Copyright (C) 2012-2014 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 
 from .common import *
 from .login import LoginByIDCommand, LoginByNumberCommand
