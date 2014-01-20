1d54d3149f7861c9fc75135ee05962c45c891d1b
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2014-01-20 13:31:11 -0500

    implement AMIEvent

---
+++ xivo-bus/xivo_bus/resources/ami/tests/test_event.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013-2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from __future__ import unicode_literals
+
+import unittest
+from ..event import AMIEvent
+
+
+class TestAMIEvent(unittest.TestCase):
+
+    def test_marshal(self):
+        name = 'EventName'
+        variables = {'a': 'b'}
+        event = AMIEvent(name, variables)
+
+        msg = event.marshal()
+
+        self.assertEqual(msg, {'name': 'EventName', 'variables': {'a': 'b'}})
+
+    def test_string_name(self):
+        name = "EventName"
+
+        event = AMIEvent(name, {})
+
+        self.assertEqual(event.name, 'EventName')
