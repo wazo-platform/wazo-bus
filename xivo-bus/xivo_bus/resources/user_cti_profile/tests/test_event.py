262a418d9ff161beedb92c3c6b4925ec7f0870cd
Jean Aunis <jaunis@avencall.com>
2014-01-27 18:42:25 +0100

    removed unused Associated/Dissociated events for UserCtiProfile

--- xivo-bus/xivo_bus/resources/user_cti_profile/tests/test_event.py
+++ xivo-bus/xivo_bus/resources/user_cti_profile/tests/test_event.py
@@ -32,16 +32,17 @@ USER_ID = 1
 CTI_PROFILE_ID = 2
 
 
-class TestUserVoicemailConfigEvent(unittest.TestCase):
+class TestUserCtiProfileEvent(unittest.TestCase):
 
     def setUp(self):
         self.msg = {
             'user_id': USER_ID,
-            'cti_profile_id': CTI_PROFILE_ID
+            'cti_profile_id': CTI_PROFILE_ID,
+            'enabled': True
         }
 
     def test_marshal(self):
-        command = ConcreteUserCtiProfileConfigEvent(USER_ID, CTI_PROFILE_ID)
+        command = ConcreteUserCtiProfileConfigEvent(USER_ID, CTI_PROFILE_ID, True)
 
         msg = command.marshal()
 
@@ -52,4 +53,5 @@ class TestUserVoicemailConfigEvent(unittest.TestCase):
 
         assert_that(event, all_of(
             has_property('user_id', USER_ID),
-            has_property('cti_profile_id', CTI_PROFILE_ID)))
+            has_property('cti_profile_id', CTI_PROFILE_ID),
+            has_property('enabled', True)))
