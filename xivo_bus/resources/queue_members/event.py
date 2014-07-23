556d47646bfbb97b9c45f449ed37622b2844d9e9
Jean Aunis <jaunis@avencall.com>
2014-07-23 14:02:49 +0200

    created an event for agent-queue association edition

---
+++ xivo_bus/resources/queue_members/event.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013-2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from __future__ import unicode_literals
+
+from xivo_bus.resources.common.event import ResourceConfigEvent
+
+
+class AgentQueueAssociationEditedEvent(ResourceConfigEvent):
+    name = 'agent_queue_association_edited'
+
+    def __init__(self, queue_id, agent_id, penalty):
+        self.queue_id = queue_id
+        self.agent_id = agent_id
+        self.penalty = penalty
+
+    def marshal(self):
+        return {
+            'queue_id': self.queue_id,
+            'agent_id': self.agent_id,
+            'penalty': self.penalty
+            }
+
+    @classmethod
+    def unmarshal(cls, msg):
+        return cls(msg['queue_id'], msg['agent_id'], msg['penalty'])
---
+++ xivo_bus/resources/queue_members/tests/test_event.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013-2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from __future__ import unicode_literals
+
+import unittest
+from hamcrest import assert_that, equal_to, has_property, all_of
+
+
+from ..event import AgentQueueAssociationEditedEvent
+
+
+AGENT_ID = 15
+QUEUE_ID = 2
+PENALTY = 5
+
+
+class TestQueueMemberConfigEvent(unittest.TestCase):
+
+    def setUp(self):
+        self.msg = {
+            'agent_id': AGENT_ID,
+            'queue_id': QUEUE_ID,
+            'penalty': PENALTY
+        }
+
+    def test_marshal(self):
+        command = AgentQueueAssociationEditedEvent(QUEUE_ID, AGENT_ID, PENALTY)
+
+        msg = command.marshal()
+
+        assert_that(msg, equal_to(self.msg))
+
+    def test_unmarshal(self):
+        event = AgentQueueAssociationEditedEvent.unmarshal(self.msg)
+
+        assert_that(event, all_of(has_property('agent_id', AGENT_ID),
+                                  has_property('queue_id', QUEUE_ID),
+                                  has_property('penalty', PENALTY)))
