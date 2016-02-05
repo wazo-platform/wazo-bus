a46aee955b9dfd9ad44820ab48e22016c75799c9
Sebastien Duthil <sduthil@avencall.com>
2016-02-05 16:38:34 -0500

    add collectd events

---
+++ xivo_bus/collectd/common/event.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+class CollectdEvent(object):
+    interval = 10
+    plugin = None
+    plugin_instance = None
+    type_ = None
+    type_instance = None
+    values = ()
+
+    def is_valid(self):
+        return (self.plugin is not None and
+                self.plugin_instance is not None and
+                self.type_ is not None and
+                self.type_instance is not None and
+                len(self.values) > 0)
