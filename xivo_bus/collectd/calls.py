05675beab196fc54c3c9cc040ecf33e4e7a5c16e
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-02-26 14:00:35 -0500

    collectd: remove all submodules in the collectd package
    common/events.py -> common.py
    channels/event.py -> channels.py
    calls/event.py -> calls.py

--- xivo_bus/collectd/calls.py
+++ xivo_bus/collectd/calls.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from xivo_bus.collectd.common.event import CollectdEvent
+from .common import CollectdEvent
 
 
 class CallCollectdEvent(CollectdEvent):
