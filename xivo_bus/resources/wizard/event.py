29a1a4bdafe9e0f4e399fc5296ced370c9a5bc86
Francois Blackburn <fblackburn@proformatique.com>
2016-04-14 07:56:15 -0400

    add wizard event

---
+++ xivo_bus/resources/wizard/event.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+class CreateWizardEvent(object):
+    name = 'wizard_created'
+    routing_key = 'config.wizard.created'
+
+    def marshal(self):
+        return {}
+
+    @classmethod
+    def unmarshal(cls, msg):
+        return cls()
+
+    def __eq__(self, other):
+        return (self.name == other.name and
+                self.routing_key == other.routing_key)
+
+    def __ne__(self, other):
+        return not self == other
