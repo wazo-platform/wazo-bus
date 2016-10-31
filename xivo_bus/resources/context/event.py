71dd280851b080b50be24d2d7eb28db5741b0317
Francois Blackburn <fblackburn@proformatique.com>
2016-10-31 14:16:39 -0400

    add new context events

---
+++ xivo_bus/resources/context/event.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Proformatique Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from __future__ import unicode_literals
+
+from xivo_bus.resources.common.event import ResourceConfigEvent
+
+
+class EditContextEvent(ResourceConfigEvent):
+    name = 'context_edited'
+    routing_key = 'config.contexts.edited'
+
+
+class CreateContextEvent(ResourceConfigEvent):
+    name = 'context_created'
+    routing_key = 'config.contexts.created'
+
+
+class DeleteContextEvent(ResourceConfigEvent):
+    name = 'context_deleted'
+    routing_key = 'config.contexts.deleted'
