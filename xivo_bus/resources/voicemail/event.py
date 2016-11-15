5a46a279ec918af66ed235cbf426c27483a3cc10
Etienne Lessard <elessard@proformatique.com>
2016-11-15 13:31:47 -0500

    voicemail: add state change event

--- xivo_bus/resources/voicemail/event.py
+++ xivo_bus/resources/voicemail/event.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2013-2014 Avencall
+# Copyright (C) 2016 Proformatique Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,6 +26,35 @@ class EditVoicemailEvent(ResourceConfigEvent):
     routing_key = 'config.voicemail.edited'
 
 
+class EditUserVoicemailEvent(object):
+    name = 'user_voicemail_edited'
+
+    def __init__(self, user_uuid, voicemail_id):
+        self.user_uuid = user_uuid
+        self.voicemail_id = int(voicemail_id)
+        self.routing_key = 'config.users.{}.voicemails.edited'.format(self.user_uuid)
+        self.required_acl = 'events.{}'.format(self.routing_key)
+
+    def marshal(self):
+        return {
+            'user_uuid': self.user_uuid,
+            'voicemail_id': self.voicemail_id,
+        }
+
+    @classmethod
+    def unmarshal(cls, msg):
+        return cls(
+            msg['user_uuid'],
+            msg['voicemail_id'])
+
+    def __eq__(self, other):
+        return (self.user_uuid == other.user_uuid and
+                self.voicemail_id == other.voicemail_id)
+
+    def __ne__(self, other):
+        return not self == other
+
+
 class CreateVoicemailEvent(ResourceConfigEvent):
     name = 'voicemail_created'
     routing_key = 'config.voicemail.created'
@@ -33,3 +63,47 @@ class CreateVoicemailEvent(ResourceConfigEvent):
 class DeleteVoicemailEvent(ResourceConfigEvent):
     name = 'voicemail_deleted'
     routing_key = 'config.voicemail.deleted'
+
+
+class _UserVoicemailMessageEvent(object):
+
+    def __init__(self, user_uuid, voicemail_id, message_id, message):
+        self.user_uuid = user_uuid
+        self.voicemail_id = voicemail_id
+        self.message_id = message_id
+        self.message = message
+        self.required_acl = 'events.users.{}.voicemails'.format(self.user_uuid)
+
+    def marshal(self):
+        return {
+            'user_uuid': self.user_uuid,
+            'voicemail_id': self.voicemail_id,
+            'message_id': self.message_id,
+            'message': self.message,
+        }
+
+    @classmethod
+    def unmarshal(cls, msg):
+        return cls(
+            msg['user_uuid'],
+            msg['voicemail_id'],
+            msg['message_id'],
+            msg['message'])
+
+
+class CreateUserVoicemailMessageEvent(_UserVoicemailMessageEvent):
+
+    name = 'user_voicemail_message_created'
+    routing_key = 'voicemails.messages.created'
+
+
+class UpdateUserVoicemailMessageEvent(_UserVoicemailMessageEvent):
+
+    name = 'user_voicemail_message_updated'
+    routing_key = 'voicemails.messages.updated'
+
+
+class DeleteUserVoicemailMessageEvent(_UserVoicemailMessageEvent):
+
+    name = 'user_voicemail_message_deleted'
+    routing_key = 'voicemails.messages.deleted'
