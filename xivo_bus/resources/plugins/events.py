4ab8eada4465a264099cc3cd9de3c3efc0139b0b
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-05-16 13:35:44 -0400

    add plugin installation events

---
+++ xivo_bus/resources/plugins/events.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+class PluginInstallProgressEvent(object):
+
+    name = 'plugin_install_progress'
+    routing_key_fmt = 'plugin.install.{uuid}.{status}'
+
+    def __init__(self, uuid_, status):
+        self._uuid = uuid_
+        self._status = status
+        self.routing_key = self.routing_key_fmt.format(uuid=uuid_, status=status)
+
+    def marshal(self):
+        return {
+            'uuid': self._uuid,
+            'status': self._status,
+        }
+
+    def __eq__(self, other):
+        return self._uuid == other._uuid and self._status == other._status
+
+    def __ne__(self, other):
+        return not self == other
+
+    @classmethod
+    def unmarshal(cls, body):
+        return cls(body['uuid'], body['status'])
---
+++ xivo_bus/resources/plugins/tests/test_events.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from unittest import TestCase
+from uuid import uuid4
+from hamcrest import assert_that, equal_to
+from ..events import PluginInstallProgressEvent
+
+
+def new_uuid():
+    return str(uuid4())
+
+
+class TestPluginInstallProgressEvent(TestCase):
+
+    def test_marshal(self):
+        uuid_ = new_uuid()
+        status = 'start'
+
+        event = PluginInstallProgressEvent(uuid_, status)
+
+        result = event.marshal()
+        expected = {'uuid': uuid_, 'status': status}
+
+        assert_that(result, equal_to(expected))
+
+    def test_unmarshal(self):
+        uuid_ = new_uuid()
+        status = 'building'
+
+        body = {'uuid': uuid_, 'status': status}
+        event = PluginInstallProgressEvent.unmarshal(body)
+        expected = PluginInstallProgressEvent(uuid_, status)
+
+        assert_that(event, equal_to(expected))
