44c295586fb38a296e0164b4f5639b0af2ba6b5a
Pascal Cadotte Michaud <pcm@wazo.io>
2017-12-04 12:00:28 -0500

    add the UserExternalAuth{Added,Deleted} events

---
+++ xivo_bus/resources/auth/events.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+
+class _BaseExternalAuthEvent(object):
+
+    def __init__(self, user_uuid, external_auth_name):
+        self._body = dict(user_uuid=str(user_uuid), external_auth_name=external_auth_name)
+        self.routing_key = self.routing_key_fmt.format(**self._body)
+        self.required_acl = 'events.{}'.format(self.routing_key)
+
+    def marshal(self):
+        return self._body
+
+    def __ne__(self, other):
+        return not self._body == other._body
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and self._body == other._body
+
+    @classmethod
+    def unmarshal(cls, body):
+        return cls(**body)
+
+
+
+
+class UserExternalAuthAdded(_BaseExternalAuthEvent):
+
+    name = 'auth_user_external_auth_added'
+    routing_key_fmt = 'auth.users.{user_uuid}.external.{external_auth_name}.created'
+
+
+class UserExternalAuthDeleted(_BaseExternalAuthEvent):
+
+    name = 'auth_user_external_auth_deleted'
+    routing_key_fmt = 'auth.users.{user_uuid}.external.{external_auth_name}.deleted'
