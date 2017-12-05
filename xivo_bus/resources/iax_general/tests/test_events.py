7c92ddec3b10c4daf8df6c8b9c2e1b8f1bdd6055
Francois Blackburn <fblackburn@wazo.community>
2017-12-05 11:05:12 -0500

    add iax_general events

---
+++ xivo_bus/resources/iax_general/tests/test_events.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from __future__ import unicode_literals
+
+import unittest
+from ..event import EditIAXGeneralEvent
+
+
+class TestEditIAXGeneralEvent(unittest.TestCase):
+
+    def setUp(self):
+        self.msg = {}
+
+    def test_marshal(self):
+        command = EditIAXGeneralEvent()
+
+        msg = command.marshal()
+
+        self.assertEqual(msg, self.msg)
+
+    def test_unmarshal(self):
+        command = EditIAXGeneralEvent.unmarshal(self.msg)
+
+        self.assertEqual(command.name, EditIAXGeneralEvent.name)
