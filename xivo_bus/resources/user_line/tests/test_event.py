ba9cbb6f07695863accb98e66b7c900737d7c032
Francois Blackburn <fblackburn@wazo.community>
2017-12-08 08:23:34 -0500

    user_line: add __eq__/__ne__ methods

--- xivo_bus/resources/user_line/tests/test_event.py
+++ xivo_bus/resources/user_line/tests/test_event.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Avencall
+# Copyright 2013-2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from __future__ import unicode_literals
 
 import unittest
-from hamcrest import assert_that, equal_to, has_property, all_of
+from hamcrest import assert_that, equal_to, has_entries
 
 
 from ..event import UserLineConfigEvent
@@ -41,8 +41,9 @@ class TestUserLineConfigEvent(unittest.TestCase):
     def test_unmarshal(self):
         event = ConcreteUserLineConfigEvent.unmarshal(self.msg)
 
-        assert_that(event, all_of(
-            has_property('user_id', USER_ID),
-            has_property('line_id', LINE_ID),
-            has_property('main_user', MAIN_USER),
-            has_property('main_line', MAIN_LINE)))
+        assert_that(event._body, has_entries(
+            user_id=USER_ID,
+            line_id=LINE_ID,
+            main_user=MAIN_USER,
+            main_line=MAIN_LINE
+        ))
