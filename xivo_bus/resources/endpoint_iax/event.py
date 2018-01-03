58047d4bee36b8db21377319af5fdbacc5649475
Francois Blackburn <fblackburn@wazo.community>
2018-01-03 10:03:47 -0500

    add iax events

---
+++ xivo_bus/resources/endpoint_iax/event.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from __future__ import unicode_literals
+
+from xivo_bus.resources.common.event import ResourceConfigEvent
+
+
+class EditIAXEndpointEvent(ResourceConfigEvent):
+    name = 'iax_endpoint_edited'
+    routing_key = 'config.iax_endpoint.edited'
+
+
+class CreateIAXEndpointEvent(ResourceConfigEvent):
+    name = 'iax_endpoint_created'
+    routing_key = 'config.iax_endpoint.created'
+
+
+class DeleteIAXEndpointEvent(ResourceConfigEvent):
+    name = 'iax_endpoint_deleted'
+    routing_key = 'config.iax_endpoint.deleted'
