08b9060eb32c11875b459444fc8ba3ffcf27d03d
Francois Blackburn <fblackburn@wazo.community>
2018-01-06 19:16:28 -0500

    add call_filter events

---
+++ xivo_bus/resources/call_filter/event.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from __future__ import unicode_literals
+
+from xivo_bus.resources.common.event import ResourceConfigEvent
+
+
+class EditCallFilterEvent(ResourceConfigEvent):
+    name = 'call_filter_edited'
+    routing_key = 'config.callfilter.edited'
+
+
+class CreateCallFilterEvent(ResourceConfigEvent):
+    name = 'call_filter_created'
+    routing_key = 'config.callfilter.created'
+
+
+class DeleteCallFilterEvent(ResourceConfigEvent):
+    name = 'call_filter_deleted'
+    routing_key = 'config.callfilter.deleted'
