9cd72edcb94b25c6469279fd20dd87d633a2b7cc
Francois Blackburn <fblackburn@wazo.community>
2018-06-27 13:50:38 -0400

    add queue_schedule events

---
+++ xivo_bus/resources/queue_schedule/event.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from __future__ import unicode_literals
+
+
+class QueueScheduleConfigEvent(object):
+
+    def __init__(self, queue_id, schedule_id):
+        self.queue_id = queue_id
+        self.schedule_id = schedule_id
+
+    def marshal(self):
+        return {
+            'queue_id': self.queue_id,
+            'schedule_id': self.schedule_id,
+        }
+
+    @classmethod
+    def unmarshal(cls, msg):
+        return cls(
+            msg['queue_id'],
+            msg['schedule_id'])
+
+    def __eq__(self, other):
+        return (self.queue_id == other.queue_id
+                and self.schedule_id == other.schedule_id)
+
+    def __ne__(self, other):
+        return not self == other
+
+
+class QueueScheduleAssociatedEvent(QueueScheduleConfigEvent):
+    name = 'queue_schedule_associated'
+    routing_key = 'config.queues.schedules.updated'
+
+
+class QueueScheduleDissociatedEvent(QueueScheduleConfigEvent):
+    name = 'queue_schedule_dissociated'
+    routing_key = 'config.queues.schedules.deleted'
