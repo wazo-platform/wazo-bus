302f2a05c9d8633c46c3ec4c976c96e212b69195
Francois Blackburn <fblackburn@wazo.community>
2018-07-18 08:26:08 -0400

    add skill-rule events

---
+++ xivo_bus/resources/skill_rule/event.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from __future__ import unicode_literals
+
+from xivo_bus.resources.common.event import ResourceConfigEvent
+
+
+class EditSkillRuleEvent(ResourceConfigEvent):
+    name = 'skill_rule_edited'
+    routing_key = 'config.queues.skillrules.edited'
+
+
+class CreateSkillRuleEvent(ResourceConfigEvent):
+    name = 'skill_rule_created'
+    routing_key = 'config.queues.skillrules.created'
+
+
+class DeleteSkillRuleEvent(ResourceConfigEvent):
+    name = 'skill_rule_deleted'
+    routing_key = 'config.queues.skillrules.deleted'
