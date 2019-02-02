f16f00b50d5bb391be968990f05dcc9342ab0467
Francois Blackburn <fblackburn@wazo.community>
2019-02-02 10:49:01 -0500

    update SPDX-License-Identifier

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from setuptools import setup
 from setuptools import find_packages
--- xivo_bus/__init__.py
+++ xivo_bus/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2013-2016 Avencall
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_bus.marshaler import CollectdMarshaler, Marshaler  # noqa
 from xivo_bus.publisher import Publisher  # noqa
