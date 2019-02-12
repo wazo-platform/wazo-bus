a8b775981a8c7ed46a0307c3f17ce5d81b5189f6
Francois Blackburn <fblackburn@wazo.community>
2019-02-12 11:35:43 -0500

    user_line: add user_uuid and tenant_uuid to events

--- xivo_bus/resources/user_line/event.py
+++ xivo_bus/resources/user_line/event.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013-2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2013-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
@@ -10,12 +10,14 @@ from xivo_bus.resources.common.event import ResourceConfigEvent
 class UserLineConfigEvent(ResourceConfigEvent):
     routing_key = 'config.user_line_association.{}'
 
-    def __init__(self, user_id, line_id, main_user, main_line):
+    def __init__(self, user_uuid, user_id, line_id, main_user, main_line, tenant_uuid):
         self._body = {
+            'user_uuid': str(user_uuid),
             'user_id': int(user_id),
             'line_id': int(line_id),
             'main_user': bool(main_user),
-            'main_line': bool(main_line)
+            'main_line': bool(main_line),
+            'tenant_uuid': str(tenant_uuid),
         }
 
     def marshal(self):
