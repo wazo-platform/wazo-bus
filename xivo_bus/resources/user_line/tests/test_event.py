0b20f4ec504909b8df74fdeb4f59d87486f0542a
Francois Blackburn <fblackburn@wazo.community>
2019-02-27 10:43:14 -0500

    rewrite user_line events

--- xivo_bus/resources/user_line/tests/test_event.py
+++ xivo_bus/resources/user_line/tests/test_event.py
@@ -15,39 +15,25 @@ from ..event import (
 )
 
 USER_UUID = str(uuid.uuid4())
-USER_ID = 1
+USER = {'uuid': USER_UUID}
 LINE_ID = 2
+LINE = {'id': LINE_ID}
 MAIN_USER = True
 MAIN_LINE = True
-TENANT_UUID = str(uuid.uuid4())
 
 
 class TestUserLineEvent(unittest.TestCase):
 
     def test_associated_routing_key_fmt(self):
-        msg = UserLineAssociatedEvent(
-            USER_UUID,
-            USER_ID,
-            LINE_ID,
-            MAIN_USER,
-            MAIN_LINE,
-            TENANT_UUID,
-        )
+        msg = UserLineAssociatedEvent(USER, LINE, MAIN_USER, MAIN_LINE)
         assert_that(
             msg.routing_key,
-            equal_to('config.user_line_association.created')
+            equal_to('config.users.{}.lines.{}.updated'.format(USER_UUID, LINE_ID))
         )
 
     def test_dissociated_routing_key_fmt(self):
-        msg = UserLineDissociatedEvent(
-            USER_UUID,
-            USER_ID,
-            LINE_ID,
-            MAIN_USER,
-            MAIN_LINE,
-            TENANT_UUID,
-        )
+        msg = UserLineDissociatedEvent(USER, LINE, MAIN_USER, MAIN_LINE)
         assert_that(
             msg.routing_key,
-            equal_to('config.user_line_association.deleted')
+            equal_to('config.users.{}.lines.{}.deleted'.format(USER_UUID, LINE_ID))
         )
