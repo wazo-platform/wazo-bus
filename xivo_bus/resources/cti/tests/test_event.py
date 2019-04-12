ca0d9ae1930a13be5595da1267768a67612da6bd
Francois Blackburn <fblackburn@wazo.community>
2019-04-12 15:43:33 -0400

    remove unused xivo-cti events

--- xivo_bus/resources/cti/tests/test_event.py
+++ xivo_bus/resources/cti/tests/test_event.py
@@ -11,31 +11,8 @@ from hamcrest import assert_that
 from hamcrest import equal_to
 
 from ..event import AgentStatusUpdateEvent
-from ..event import CallFormResultEvent
-from ..event import EndpointStatusUpdateEvent
-
 
 SOME_UUID = str(uuid.uuid4())
-
-
-class TestCallFormResultEvent(unittest.TestCase):
-
-    def test_marshal(self):
-        user_id = 42
-        variables = {'a': 'b'}
-        event = CallFormResultEvent(user_id, variables)
-
-        msg = event.marshal()
-
-        assert_that(msg, equal_to({'user_id': 42,
-                                   'variables': {'a': 'b'}}))
-
-    def test_string_user_id(self):
-        user_id = "42"
-
-        event = CallFormResultEvent(user_id, {})
-
-        assert_that(event.user_id, equal_to(42))
 
 
 class TestAgentStatusUpdateEvent(unittest.TestCase):
@@ -59,36 +36,3 @@ class TestAgentStatusUpdateEvent(unittest.TestCase):
 
         assert_that(msg, equal_to({'agent_id': 42,
                                    'status': 'logged_in'}))
-
-
-class TestEndpointStatusUpdateEvent(unittest.TestCase):
-
-    def test_marshal(self):
-        endpoint_id = 42
-        status = 8
-
-        event = EndpointStatusUpdateEvent(endpoint_id, status)
-        msg = event.marshal()
-
-        assert_that(msg, equal_to({'endpoint_id': endpoint_id,
-                                   'status': status}))
-
-    def test_marshal_with_string_status(self):
-        endpoint_id = 42
-
-        event = EndpointStatusUpdateEvent(endpoint_id, '8')
-        msg = event.marshal()
-
-        assert_that(msg, equal_to({'endpoint_id': endpoint_id,
-                                   'status': 8}))
-
-    def test_that_string_endpoint_ids_are_not_leaked(self):
-        endpoint_id = '42'
-        status = 8
-
-        event = EndpointStatusUpdateEvent(endpoint_id, status)
-
-        msg = event.marshal()
-
-        assert_that(msg, equal_to({'endpoint_id': 42,
-                                   'status': status}))
