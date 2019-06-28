4740abe2e7832011fcd657e73292dd4320336af5
Francois Blackburn <fblackburn@wazo.community>
2019-06-28 14:06:02 -0400

    application: add fields to events

--- xivo_bus/resources/application/event.py
+++ xivo_bus/resources/application/event.py
@@ -1,39 +1,29 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
 
-
-class BaseApplicationEvent(object):
-
-    def __init__(self, app_uuid):
-        self.uuid = str(app_uuid)
-
-    def marshal(self):
-        return {'uuid': self.uuid}
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(msg['uuid'])
-
-    def __eq__(self, other):
-        return self.uuid == other.uuid
-
-    def __ne__(self, other):
-        return self.uuid != other.uuid
+from ..common.event import BaseEvent
 
 
-class EditApplicationEvent(BaseApplicationEvent):
+class _BaseApplicationEvent(BaseEvent):
+
+    def __init__(self, application):
+        self._body = application
+        super(_BaseApplicationEvent, self).__init__()
+
+
+class EditApplicationEvent(_BaseApplicationEvent):
     name = 'application_edited'
-    routing_key = 'config.applications.edited'
+    routing_key_fmt = 'config.applications.edited'
 
 
-class CreateApplicationEvent(BaseApplicationEvent):
+class CreateApplicationEvent(_BaseApplicationEvent):
     name = 'application_created'
-    routing_key = 'config.applications.created'
+    routing_key_fmt = 'config.applications.created'
 
 
-class DeleteApplicationEvent(BaseApplicationEvent):
+class DeleteApplicationEvent(_BaseApplicationEvent):
     name = 'application_deleted'
-    routing_key = 'config.applications.deleted'
+    routing_key_fmt = 'config.applications.deleted'
