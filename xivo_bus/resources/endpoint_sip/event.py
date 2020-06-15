a8501635526839e77729159e01c28f46f264ea23
Pascal Cadotte Michaud <pcm@wazo.io>
2020-06-15 13:45:29 -0400

    endpoint_sip: fix routing key format strings

--- xivo_bus/resources/endpoint_sip/event.py
+++ xivo_bus/resources/endpoint_sip/event.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
@@ -12,6 +12,21 @@ class _BaseSipEndpointEvent(BaseEvent):
     def __init__(self, endpoint_sip):
         self._body = endpoint_sip
         super(_BaseSipEndpointEvent, self).__init__()
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+
+        return self.__class__ == other.__class__ and self._body == other._body
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, self._body)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class EditSipEndpointEvent(_BaseSipEndpointEvent):
