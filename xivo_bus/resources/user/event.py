252f7f7579e9958f7cb62c5fda8bf4a0f63f1fa4
Alexandre Fournier <afournier@wazo.io>
2020-07-31 16:04:23 -0400

    user: add tenant_uuid to Services and Forwards events

--- xivo_bus/resources/user/event.py
+++ xivo_bus/resources/user/event.py
@@ -59,20 +59,25 @@ class EditUserFallbackEvent(ResourceWithUUIDConfigEvent):
 
 
 class _BaseConfigUserEvent(object):
-    def __init__(self, user_id, user_uuid):
+    def __init__(self, user_id, user_uuid, tenant_uuid):
         self.user_id = user_id
         self.user_uuid = user_uuid
+        self.tenant_uuid = tenant_uuid
 
     def __eq__(self, other):
-        return self.user_uuid == other.user_uuid and self.user_id == other.user_id
+        return (
+            self.user_uuid == other.user_uuid
+            and self.user_id == other.user_id
+            and self.tenant_uuid == other.tenant_uuid
+        )
 
     def __ne__(self, other):
         return not self == other
 
 
 class EditUserServiceEvent(_BaseConfigUserEvent):
-    def __init__(self, user_id, user_uuid, service_name, service_enabled):
-        super(EditUserServiceEvent, self).__init__(user_id, user_uuid)
+    def __init__(self, user_id, user_uuid, tenant_uuid, service_name, service_enabled):
+        super(EditUserServiceEvent, self).__init__(user_id, user_uuid, tenant_uuid)
         self.service_enabled = service_enabled
         self.name = 'users_services_{}_updated'.format(service_name)
         self.routing_key = 'config.users.{}.services.{}.updated'.format(user_uuid, service_name)
@@ -82,6 +87,7 @@ class EditUserServiceEvent(_BaseConfigUserEvent):
         return {
             'user_id': self.user_id,
             'user_uuid': self.user_uuid,
+            'tenant_uuid': self.tenant_uuid,
             'enabled': self.service_enabled
         }
 
@@ -89,12 +95,13 @@ class EditUserServiceEvent(_BaseConfigUserEvent):
     def unmarshal(cls, msg):
         return cls(msg['user_id'],
                    msg['user_uuid'],
+                   msg['tenant_uuid'],
                    msg['enabled'])
 
 
 class EditUserForwardEvent(_BaseConfigUserEvent):
-    def __init__(self, user_id, user_uuid, forward_name, forward_enabled, forward_destination):
-        super(EditUserForwardEvent, self).__init__(user_id, user_uuid)
+    def __init__(self, user_id, user_uuid, tenant_uuid, forward_name, forward_enabled, forward_destination):
+        super(EditUserForwardEvent, self).__init__(user_id, user_uuid, tenant_uuid)
         self.forward_enabled = forward_enabled
         self.forward_destination = forward_destination
         self.name = 'users_forwards_{}_updated'.format(forward_name)
@@ -105,6 +112,7 @@ class EditUserForwardEvent(_BaseConfigUserEvent):
         return {
             'user_id': self.user_id,
             'user_uuid': self.user_uuid,
+            'tenant_uuid': self.tenant_uuid,
             'enabled': self.forward_enabled,
             'destination': self.forward_destination
         }
@@ -113,5 +121,6 @@ class EditUserForwardEvent(_BaseConfigUserEvent):
     def unmarshal(cls, msg):
         return cls(msg['user_id'],
                    msg['user_uuid'],
+                   msg['tenant_uuid'],
                    msg['enabled'],
                    msg['destination'])
