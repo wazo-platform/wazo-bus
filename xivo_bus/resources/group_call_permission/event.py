86231a6df94125e2221f09faaab9f408563fc6ff
Pascal Cadotte Michaud <pcm@wazo.io>
2021-02-25 14:37:10 -0500

    group_call_permission: add group_uuid to the event body

--- xivo_bus/resources/group_call_permission/event.py
+++ xivo_bus/resources/group_call_permission/event.py
@@ -1,51 +1,24 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
 
-from xivo_bus.resources.common.event import ResourceConfigEvent
+from xivo_bus.resources.common.event import BaseEvent
 
 
-class GroupCallPermissionConfigEvent(ResourceConfigEvent):
+class GroupCallPermissionConfigEvent(BaseEvent):
 
-    def __init__(self, group_id, call_permission_id):
-        self.group_id = group_id
-        self.call_permission_id = call_permission_id
-
-    def marshal(self):
-        return {
-            'group_id': self.group_id,
-            'call_permission_id': self.call_permission_id,
-        }
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(
-            msg['group_id'],
-            msg['call_permission_id'])
-
-    def __eq__(self, other):
-        return (self.group_id == other.group_id
-                and self.call_permission_id == other.call_permission_id)
-
-    def __ne__(self, other):
-        return not self == other
+    def __init__(self, **body):
+        self._body = body
+        super(GroupCallPermissionConfigEvent, self).__init__()
 
 
 class GroupCallPermissionAssociatedEvent(GroupCallPermissionConfigEvent):
     name = 'call_permission_associated'
-
-    def __init__(self, group_id, call_permission_id):
-        super(GroupCallPermissionAssociatedEvent, self).__init__(group_id, call_permission_id)
-        self.routing_key = 'config.groups.{}.callpermissions.updated'.format(self.group_id)
-        self.required_acl = 'events.{}'.format(self.routing_key)
+    routing_key_fmt = 'config.groups.{group_uuid}.callpermissions.updated'
 
 
 class GroupCallPermissionDissociatedEvent(GroupCallPermissionConfigEvent):
     name = 'call_permission_dissociated'
-
-    def __init__(self, group_id, call_permission_id):
-        super(GroupCallPermissionDissociatedEvent, self).__init__(group_id, call_permission_id)
-        self.routing_key = 'config.groups.{}.callpermissions.deleted'.format(self.group_id)
-        self.required_acl = 'events.{}'.format(self.routing_key)
+    routing_key_fmt = 'config.groups.{group_uuid}.callpermissions.deleted'
