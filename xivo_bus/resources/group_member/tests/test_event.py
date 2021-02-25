dc1321498dd96e61c166a2435618e36af0b2ffc9
Pascal Cadotte Michaud <pcm@wazo.io>
2021-02-25 15:04:28 -0500

    group members: add group_uuid to events

--- xivo_bus/resources/group_member/tests/test_event.py
+++ xivo_bus/resources/group_member/tests/test_event.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
 
 import unittest
-from hamcrest import assert_that, equal_to, has_property, all_of
+from uuid import uuid4
+from hamcrest import assert_that, equal_to
 
 
 from ..event import GroupMemberUsersAssociatedEvent, GroupMemberExtensionsAssociatedEvent
 
 
 GROUP_ID = 5
+GROUP_UUID = str(uuid4())
 USER_UUIDS = ['abcd-123', 'efg-456', 'hij-789']
 EXTENSIONS = [{'exten': '123', 'context': 'default'}, {'exten': '456', 'context': 'default2'}]
 
@@ -21,11 +23,16 @@ class TestGroupMemberUserConfigEvent(unittest.TestCase):
     def setUp(self):
         self.msg = {
             'group_id': GROUP_ID,
+            'group_uuid': GROUP_UUID,
             'user_uuids': USER_UUIDS,
         }
 
     def test_marshal(self):
-        command = GroupMemberUsersAssociatedEvent(GROUP_ID, USER_UUIDS)
+        command = GroupMemberUsersAssociatedEvent(
+            group_id=GROUP_ID,
+            group_uuid=GROUP_UUID,
+            user_uuids=USER_UUIDS,
+        )
 
         msg = command.marshal()
 
@@ -34,9 +41,7 @@ class TestGroupMemberUserConfigEvent(unittest.TestCase):
     def test_unmarshal(self):
         event = GroupMemberUsersAssociatedEvent.unmarshal(self.msg)
 
-        assert_that(event, all_of(
-            has_property('group_id', GROUP_ID),
-            has_property('user_uuids', USER_UUIDS)))
+        assert_that(event._body, equal_to(self.msg))
 
 
 class TestGroupMemberExtensionConfigEvent(unittest.TestCase):
@@ -44,19 +49,22 @@ class TestGroupMemberExtensionConfigEvent(unittest.TestCase):
     def setUp(self):
         self.msg = {
             'group_id': GROUP_ID,
+            'group_uuid': GROUP_UUID,
             'extensions': EXTENSIONS,
         }
 
     def test_marshal(self):
-        command = GroupMemberExtensionsAssociatedEvent(GROUP_ID, EXTENSIONS)
+        event = GroupMemberExtensionsAssociatedEvent(
+            group_id=GROUP_ID,
+            group_uuid=GROUP_UUID,
+            extensions=EXTENSIONS,
+        )
 
-        msg = command.marshal()
+        msg = event.marshal()
 
         assert_that(msg, equal_to(self.msg))
 
     def test_unmarshal(self):
         event = GroupMemberExtensionsAssociatedEvent.unmarshal(self.msg)
 
-        assert_that(event, all_of(
-            has_property('group_id', GROUP_ID),
-            has_property('extensions', EXTENSIONS)))
+        assert_that(event._body, equal_to(self.msg))
