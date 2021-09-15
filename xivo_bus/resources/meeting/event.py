e0964923931f032b27dbfa35c2c7c8a4f1a9c3ab
Sebastien Duthil <sduthil@wazo.community>
2021-09-15 17:53:25 -0400

    meetings: add join/left events

--- xivo_bus/resources/meeting/event.py
+++ xivo_bus/resources/meeting/event.py
@@ -7,7 +7,6 @@ from ..common.event import BaseEvent
 
 
 class _BaseMeetingEvent(BaseEvent):
-
     def __init__(self, meeting):
         self._body = meeting
         super(_BaseMeetingEvent, self).__init__()
@@ -26,3 +25,64 @@ class EditMeetingEvent(_BaseMeetingEvent):
 class DeleteMeetingEvent(_BaseMeetingEvent):
     name = 'meeting_deleted'
     routing_key_fmt = 'config.meetings.deleted'
+
+
+class _BaseParticipantMeetingEvent(BaseEvent):
+    def __init__(self, meeting_uuid, participant_dict):
+        self._body = {'meeting_uuid': meeting_uuid}
+        self._body.update(participant_dict)
+        super(_BaseParticipantMeetingEvent, self).__init__()
+
+
+class ParticipantJoinedMeetingEvent(_BaseParticipantMeetingEvent):
+    name = 'meeting_participant_joined'
+    routing_key_fmt = 'meetings.{meeting_uuid}.participants.joined'
+
+    def __init__(self, meeting_uuid, participant_dict):
+        super(ParticipantJoinedMeetingEvent, self).__init__(
+            meeting_uuid, participant_dict
+        )
+
+
+class ParticipantLeftMeetingEvent(_BaseParticipantMeetingEvent):
+    name = 'meeting_participant_left'
+    routing_key_fmt = 'meetings.{meeting_uuid}.participants.left'
+
+    def __init__(self, meeting_uuid, participant_dict):
+        super(ParticipantLeftMeetingEvent, self).__init__(
+            meeting_uuid, participant_dict
+        )
+
+
+class UserParticipantJoinedMeetingEvent(_BaseParticipantMeetingEvent):
+    name = 'meeting_user_participant_joined'
+    routing_key_fmt = 'meetings.users.{user_uuid}.participants.joined'
+
+    def __init__(self, meeting_uuid, participant_dict, user_uuid=None):
+        super(UserParticipantJoinedMeetingEvent, self).__init__(
+            meeting_uuid, participant_dict
+        )
+        if user_uuid:
+            self.routing_key = self.routing_key_fmt.format(user_uuid=user_uuid)
+            self.required_acl = (
+                'events.users.{user_uuid}.meetings.participants.joined'.format(
+                    user_uuid=user_uuid
+                )
+            )
+
+
+class UserParticipantLeftMeetingEvent(_BaseParticipantMeetingEvent):
+    name = 'meeting_user_participant_left'
+    routing_key_fmt = 'meetings.users.{user_uuid}.participants.left'
+
+    def __init__(self, meeting_uuid, participant_dict, user_uuid=None):
+        super(UserParticipantLeftMeetingEvent, self).__init__(
+            meeting_uuid, participant_dict
+        )
+        if user_uuid:
+            self.routing_key = self.routing_key_fmt.format(user_uuid=user_uuid)
+            self.required_acl = (
+                'events.users.{user_uuid}.meetings.participants.left'.format(
+                    user_uuid=user_uuid
+                )
+            )
