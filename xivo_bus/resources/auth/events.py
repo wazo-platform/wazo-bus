13f5b33576cfd1403f5c6559a2f08c7db7720bea
Julien Alie <jalie@wazo.io>
2022-07-04 16:06:41 -0400

    update `wazo-auth` events

--- xivo_bus/resources/auth/events.py
+++ xivo_bus/resources/auth/events.py
@@ -1,133 +1,131 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from ..common.event import BaseEvent
+from xivo_bus.resources.common.event import TenantEvent, UserEvent
 
 
-class _BaseExternalAuthEvent(BaseEvent):
-
-    def __init__(self, user_uuid, external_auth_name):
-        self._body = dict(user_uuid=str(user_uuid), external_auth_name=external_auth_name)
-        super(_BaseExternalAuthEvent, self).__init__()
-
-
-class TenantCreatedEvent(BaseEvent):
-
+class TenantCreatedEvent(TenantEvent):
     name = 'auth_tenant_added'
-    routing_key_fmt = 'auth.tenants.{uuid}.created'
+    routing_key_fmt = 'auth.tenants.{tenant_uuid}.created'
 
-    def __init__(self, **body):
-        self._body = body
-        super(TenantCreatedEvent, self).__init__()
+    def __init__(self, tenant_data, tenant_uuid):
+        super(TenantCreatedEvent, self).__init__(tenant_data, tenant_uuid)
 
 
-class TenantUpdatedEvent(BaseEvent):
-
+class TenantUpdatedEvent(TenantEvent):
     name = 'auth_tenant_updated'
-    routing_key_fmt = 'auth.tenants.{uuid}.updated'
+    routing_key_fmt = 'auth.tenants.{tenant_uuid}.updated'
 
-    def __init__(self, uuid, name):
-        self._body = {'uuid': uuid, 'name': name}
-        super(TenantUpdatedEvent, self).__init__()
+    def __init__(self, name, tenant_uuid):
+        content = {'uuid': tenant_uuid, 'name': name}
+        super(TenantUpdatedEvent, self).__init__(content, tenant_uuid)
 
 
-class TenantDeletedEvent(BaseEvent):
-
+class TenantDeletedEvent(TenantEvent):
     name = 'auth_tenant_deleted'
-    routing_key_fmt = 'auth.tenants.{uuid}.deleted'
+    routing_key_fmt = 'auth.tenants.{tenant_uuid}.deleted'
 
-    def __init__(self, uuid):
-        self._body = {'uuid': uuid}
-        super(TenantDeletedEvent, self).__init__()
+    def __init__(self, tenant_uuid):
+        content = {'uuid': tenant_uuid}
+        super(TenantDeletedEvent, self).__init__(content, tenant_uuid)
 
 
-class UserExternalAuthAdded(_BaseExternalAuthEvent):
-
+class UserExternalAuthAddedEvent(UserEvent):
     name = 'auth_user_external_auth_added'
     routing_key_fmt = 'auth.users.{user_uuid}.external.{external_auth_name}.created'
 
+    def __init__(self, external_auth_name, tenant_uuid, user_uuid):
+        content = {'user_uuid': user_uuid, 'external_auth_name': external_auth_name}
+        super(UserExternalAuthAddedEvent, self).__init__(content, tenant_uuid, user_uuid)
 
-class UserExternalAuthAuthorized(_BaseExternalAuthEvent):
 
+class UserExternalAuthAuthorizedEvent(UserEvent):
     name = 'auth_user_external_auth_authorized'
     routing_key_fmt = 'auth.users.{user_uuid}.external.{external_auth_name}.authorized'
 
+    def __init__(self, external_auth_name, tenant_uuid, user_uuid):
+        content = {'user_uuid': user_uuid, 'external_auth_name': external_auth_name}
+        super(UserExternalAuthAuthorizedEvent, self).__init__(
+            content, tenant_uuid, user_uuid
+        )
 
-class UserExternalAuthDeleted(_BaseExternalAuthEvent):
 
+class UserExternalAuthDeletedEvent(UserEvent):
     name = 'auth_user_external_auth_deleted'
     routing_key_fmt = 'auth.users.{user_uuid}.external.{external_auth_name}.deleted'
 
+    def __init__(self, external_auth_name, tenant_uuid, user_uuid):
+        content = {'user_uuid': user_uuid, 'external_auth_name': external_auth_name}
+        super(UserExternalAuthDeletedEvent, self).__init__(content, tenant_uuid, user_uuid)
 
-class RefreshTokenCreatedEvent(BaseEvent):
 
+class RefreshTokenCreatedEvent(UserEvent):
     name = 'auth_refresh_token_created'
     routing_key_fmt = 'auth.users.{user_uuid}.tokens.{client_id}.created'
 
-    def __init__(self, user_uuid, client_id, mobile, tenant_uuid, **ignored):
-        self._body = {
-            'user_uuid': user_uuid,
+    def __init__(self, client_id, is_mobile, tenant_uuid, user_uuid):
+        content = {
             'client_id': client_id,
+            'mobile': bool(is_mobile),
+            'user_uuid': user_uuid,
             'tenant_uuid': tenant_uuid,
-            'mobile': mobile,
         }
-        super(RefreshTokenCreatedEvent, self).__init__()
+        super(RefreshTokenCreatedEvent, self).__init__(content, tenant_uuid, user_uuid)
 
 
-class RefreshTokenDeletedEvent(BaseEvent):
-
+class RefreshTokenDeletedEvent(UserEvent):
     name = 'auth_refresh_token_deleted'
     routing_key_fmt = 'auth.users.{user_uuid}.tokens.{client_id}.deleted'
 
-    def __init__(self, user_uuid, client_id, mobile, tenant_uuid, **ignored):
-        self._body = {
-            'user_uuid': user_uuid,
+    def __init__(self, client_id, is_mobile, tenant_uuid, user_uuid):
+        content = {
             'client_id': client_id,
+            'mobile': bool(is_mobile),
+            'user_uuid': user_uuid,
             'tenant_uuid': tenant_uuid,
-            'mobile': mobile,
         }
-        super(RefreshTokenDeletedEvent, self).__init__()
+        super(RefreshTokenDeletedEvent, self).__init__(content, tenant_uuid, user_uuid)
 
 
-class SessionCreatedEvent(BaseEvent):
-
+class SessionCreatedEvent(UserEvent):
     name = 'auth_session_created'
-    routing_key_fmt = 'auth.sessions.{uuid}.created'
+    routing_key_fmt = 'auth.sessions.{session_uuid}.created'
 
-    def __init__(self, uuid, tenant_uuid, user_uuid, **kwargs):
-        self._body = {
-            'uuid': uuid,
+    def __init__(self, session_uuid, is_mobile, tenant_uuid, user_uuid):
+        content = {
+            'uuid': session_uuid,
             'tenant_uuid': tenant_uuid,
             'user_uuid': user_uuid,
-            'mobile': kwargs.get('mobile', False),
+            'mobile': bool(is_mobile),
         }
-        super(SessionCreatedEvent, self).__init__()
+        super(SessionCreatedEvent, self).__init__(content, tenant_uuid, user_uuid)
+        self.session_uuid = str(session_uuid)
 
 
-class SessionDeletedEvent(BaseEvent):
-
+class SessionDeletedEvent(UserEvent):
     name = 'auth_session_deleted'
-    routing_key_fmt = 'auth.sessions.{uuid}.deleted'
+    routing_key_fmt = 'auth.sessions.{session_uuid}.deleted'
 
-    def __init__(self, uuid, tenant_uuid, user_uuid):
-        self._body = {
-            'uuid': uuid,
-            'tenant_uuid': tenant_uuid,
+    def __init__(self, session_uuid, tenant_uuid, user_uuid):
+        content = {
+            'uuid': session_uuid,
             'user_uuid': user_uuid,
+            'tenant_uuid': tenant_uuid,
         }
-        super(SessionDeletedEvent, self).__init__()
+        super(SessionDeletedEvent, self).__init__(content, tenant_uuid, user_uuid)
+        self.session_uuid = str(session_uuid)
 
 
-class SessionExpireSoonEvent(BaseEvent):
-
+class SessionExpireSoonEvent(UserEvent):
     name = 'auth_session_expire_soon'
-    routing_key_fmt = 'auth.users.{user_uuid}.sessions.{uuid}.expire_soon'
+    routing_key_fmt = 'auth.users.{user_uuid}.sessions.{session_uuid}.expire_soon'
 
-    def __init__(self, uuid, tenant_uuid, user_uuid):
-        self._body = {
-            'uuid': uuid,
-            'tenant_uuid': tenant_uuid,
+    def __init__(self, session_uuid, tenant_uuid, user_uuid):
+        content = {
+            'uuid': session_uuid,
             'user_uuid': user_uuid,
+            'tenant_uuid': tenant_uuid,
         }
-        super(SessionExpireSoonEvent, self).__init__()
+        super(SessionExpireSoonEvent, self).__init__(content, tenant_uuid, user_uuid)
+        self.session_uuid = str(session_uuid)
