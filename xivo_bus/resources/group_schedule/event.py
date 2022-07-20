3aa32940031e574f99ded6f680e9aeb469288fe5
Julien Alie <jalie@wazo.io>
2022-07-20 16:22:22 -0400

    update group_schedule events

--- xivo_bus/resources/group_schedule/event.py
+++ xivo_bus/resources/group_schedule/event.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import BaseEvent
-
-
-class GroupScheduleConfigEvent(BaseEvent):
-
-    def __init__(self, **body):
-        self._body = body
-        super(GroupScheduleConfigEvent, self).__init__()
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class GroupScheduleAssociatedEvent(GroupScheduleConfigEvent):
+class GroupScheduleAssociatedEvent(TenantEvent):
     name = 'group_schedule_associated'
     routing_key_fmt = 'config.groups.schedules.updated'
 
+    def __init__(self, group_id, group_uuid, schedule_id, tenant_uuid):
+        content = {
+            'group_id': group_id,
+            'group_uuid': str(group_uuid),
+            'schedule_id': schedule_id,
+        }
+        super(GroupScheduleAssociatedEvent, self).__init__(content, tenant_uuid)
 
-class GroupScheduleDissociatedEvent(GroupScheduleConfigEvent):
+
+class GroupScheduleDissociatedEvent(TenantEvent):
     name = 'group_schedule_dissociated'
     routing_key_fmt = 'config.groups.schedules.deleted'
+
+    def __init__(self, group_id, group_uuid, schedule_id, tenant_uuid):
+        content = {
+            'group_id': group_id,
+            'group_uuid': str(group_uuid),
+            'schedule_id': schedule_id,
+        }
+        super(GroupScheduleDissociatedEvent, self).__init__(content, tenant_uuid)
