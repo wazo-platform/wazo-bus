74ecb2f0d72bbe5496066501028cc26a8c091788
Julien Alie <jalie@wazo.io>
2022-07-20 16:38:07 -0400

    update incall_schedules events

--- xivo_bus/resources/incall_schedule/event.py
+++ xivo_bus/resources/incall_schedule/event.py
@@ -1,41 +1,30 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class IncallScheduleConfigEvent(object):
-
-    def __init__(self, incall_id, schedule_id):
-        self.incall_id = incall_id
-        self.schedule_id = schedule_id
-
-    def marshal(self):
-        return {
-            'incall_id': self.incall_id,
-            'schedule_id': self.schedule_id,
-        }
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(
-            msg['incall_id'],
-            msg['schedule_id'])
-
-    def __eq__(self, other):
-        return (self.incall_id == other.incall_id
-                and self.schedule_id == other.schedule_id)
-
-    def __ne__(self, other):
-        return not self == other
-
-
-class IncallScheduleAssociatedEvent(IncallScheduleConfigEvent):
+class IncallScheduleAssociatedEvent(TenantEvent):
     name = 'incall_schedule_associated'
-    routing_key = 'config.incalls.schedules.updated'
+    routing_key_fmt = 'config.incalls.schedules.updated'
+
+    def __init__(self, incall_id, schedule_id, tenant_uuid):
+        content = {
+            'incall_id': incall_id,
+            'schedule_id': schedule_id,
+        }
+        super(IncallScheduleAssociatedEvent, self).__init__(content, tenant_uuid)
 
 
-class IncallScheduleDissociatedEvent(IncallScheduleConfigEvent):
+class IncallScheduleDissociatedEvent(TenantEvent):
     name = 'incall_schedule_dissociated'
-    routing_key = 'config.incalls.schedules.deleted'
+    routing_key_fmt = 'config.incalls.schedules.deleted'
+
+    def __init__(self, incall_id, schedule_id, tenant_uuid):
+        content = {
+            'incall_id': incall_id,
+            'schedule_id': schedule_id,
+        }
+        super(IncallScheduleDissociatedEvent, self).__init__(content, tenant_uuid)
