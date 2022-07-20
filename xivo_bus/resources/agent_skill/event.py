cca8bd2212d55bff149bdd24c2d99cd1a2b4df4b
Julien Alie <jalie@wazo.io>
2022-07-20 11:03:27 -0400

    update agent_skill events

--- xivo_bus/resources/agent_skill/event.py
+++ xivo_bus/resources/agent_skill/event.py
@@ -1,44 +1,30 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class AgentSkillConfigEvent(object):
-
-    def __init__(self, agent_id, skill_id):
-        self.agent_id = agent_id
-        self.skill_id = skill_id
-
-    def marshal(self):
-        return {
-            'agent_id': self.agent_id,
-            'skill_id': self.skill_id,
-        }
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(
-            msg['agent_id'],
-            msg['skill_id']
-        )
-
-    def __eq__(self, other):
-        return (
-            self.agent_id == other.agent_id
-            and self.skill_id == other.skill_id
-        )
-
-    def __ne__(self, other):
-        return not self == other
-
-
-class AgentSkillAssociatedEvent(AgentSkillConfigEvent):
+class AgentSkillAssociatedEvent(TenantEvent):
     name = 'agent_skill_associated'
-    routing_key = 'config.agents.skills.updated'
+    routing_key_fmt = 'config.agents.skills.updated'
+
+    def __init__(self, agent_id, skill_id, tenant_uuid):
+        content = {
+            'agent_id': agent_id,
+            'skill_id': skill_id,
+        }
+        super(AgentSkillAssociatedEvent, self).__init__(content, tenant_uuid)
 
 
-class AgentSkillDissociatedEvent(AgentSkillConfigEvent):
+class AgentSkillDissociatedEvent(TenantEvent):
     name = 'agent_skill_dissociated'
-    routing_key = 'config.agents.skills.deleted'
+    routing_key_fmt = 'config.agents.skills.deleted'
+
+    def __init__(self, agent_id, skill_id, tenant_uuid):
+        content = {
+            'agent_id': agent_id,
+            'skill_id': skill_id,
+        }
+        super(AgentSkillDissociatedEvent, self).__init__(content, tenant_uuid)
