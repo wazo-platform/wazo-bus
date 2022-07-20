a0a37f46030c6dfe567314a532eee32a989dbf1f
Julien Alie <jalie@wazo.io>
2022-07-20 15:59:36 -0400

    update groups events

--- xivo_bus/resources/group/event.py
+++ xivo_bus/resources/group/event.py
@@ -1,34 +1,38 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import BaseEvent
-
-
-class _BaseGroupEvent(BaseEvent):
-
-    def __init__(self, **group):
-        self._body = group
-        super(_BaseGroupEvent, self).__init__()
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class EditGroupEvent(_BaseGroupEvent):
-    name = 'group_edited'
-    routing_key_fmt = 'config.groups.edited'
-
-
-class CreateGroupEvent(_BaseGroupEvent):
+class GroupCreatedEvent(TenantEvent):
     name = 'group_created'
     routing_key_fmt = 'config.groups.created'
 
+    def __init__(self, group, tenant_uuid):
+        super(GroupCreatedEvent, self).__init__(group, tenant_uuid)
 
-class DeleteGroupEvent(_BaseGroupEvent):
+
+class GroupDeletedEvent(TenantEvent):
     name = 'group_deleted'
     routing_key_fmt = 'config.groups.deleted'
 
+    def __init__(self, group, tenant_uuid):
+        super(GroupDeletedEvent, self).__init__(group, tenant_uuid)
 
-class EditGroupFallbackEvent(_BaseGroupEvent):
+
+class GroupEditedEvent(TenantEvent):
+    name = 'group_edited'
+    routing_key_fmt = 'config.groups.edited'
+
+    def __init__(self, group, tenant_uuid):
+        super(GroupEditedEvent, self).__init__(group, tenant_uuid)
+
+
+class GroupFallbackEditedEvent(TenantEvent):
     name = 'group_fallback_edited'
     routing_key_fmt = 'config.groups.fallbacks.edited'
+
+    def __init__(self, group, tenant_uuid):
+        super(GroupFallbackEditedEvent, self).__init__(group, tenant_uuid)
