bcee44f28074fe7c484f1d0b686f31696a2f71cb
Julien Alie <jalie@wazo.io>
2022-07-20 11:54:55 -0400

    update call_filter events

--- xivo_bus/resources/call_filter/event.py
+++ xivo_bus/resources/call_filter/event.py
@@ -1,27 +1,42 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditCallFilterEvent(ResourceConfigEvent):
-    name = 'call_filter_edited'
-    routing_key = 'config.callfilter.edited'
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class CreateCallFilterEvent(ResourceConfigEvent):
+class CallFilterCreatedEvent(TenantEvent):
     name = 'call_filter_created'
-    routing_key = 'config.callfilter.created'
+    routing_key_fmt = 'config.callfilter.created'
+
+    def __init__(self, call_filter_id, tenant_uuid):
+        content = {'id': call_filter_id}
+        super(CallFilterCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteCallFilterEvent(ResourceConfigEvent):
+class CallFilterDeletedEvent(TenantEvent):
     name = 'call_filter_deleted'
-    routing_key = 'config.callfilter.deleted'
+    routing_key_fmt = 'config.callfilter.deleted'
+
+    def __init__(self, call_filter_id, tenant_uuid):
+        content = {'id': call_filter_id}
+        super(CallFilterCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class EditCallFilterFallbackEvent(ResourceConfigEvent):
+class CallFilterEditedEvent(TenantEvent):
+    name = 'call_filter_edited'
+    routing_key_fmt = 'config.callfilter.edited'
+
+    def __init__(self, call_filter_id, tenant_uuid):
+        content = {'id': call_filter_id}
+        super(CallFilterCreatedEvent, self).__init__(content, tenant_uuid)
+
+
+class CallFilterFallbackEditedEvent(TenantEvent):
     name = 'call_filter_fallback_edited'
-    routing_key = 'config.callfilters.fallbacks.edited'
+    routing_key_fmt = 'config.callfilters.fallbacks.edited'
+
+    def __init__(self, call_filter_id, tenant_uuid):
+        content = {'id': call_filter_id}
+        super(CallFilterFallbackEditedEvent, self).__init__(content, tenant_uuid)
