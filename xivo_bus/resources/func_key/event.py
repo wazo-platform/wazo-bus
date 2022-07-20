16ae063df5486e77bf5d927d490839297ff44220
Julien Alie <jalie@wazo.io>
2022-07-20 15:54:37 -0400

    remove func_key unused event classes

--- xivo_bus/resources/func_key/event.py
+++ xivo_bus/resources/func_key/event.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-from xivo_bus.resources.common.event import TenantEvent, ResourceConfigEvent
+from xivo_bus.resources.common.event import TenantEvent
 
 
 class FuncKeyTemplateCreatedEvent(TenantEvent):
@@ -31,59 +31,3 @@ class FuncKeyTemplateEditedEvent(TenantEvent):
     def __init__(self, template_id, tenant_uuid):
         content = {'id': template_id}
         super(FuncKeyTemplateEditedEvent, self).__init__(content, tenant_uuid)
-
-
-class UserFuncKeyEvent(ResourceConfigEvent):
-    routing_key = 'config.user.{}'
-
-    def __init__(self, func_key_id, user_id):
-        self.func_key_id = func_key_id
-        self.user_id = user_id
-
-    def marshal(self):
-        return {'id': self.func_key_id,
-                'destination': 'user',
-                'user_id': self.user_id}
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(msg['id'], msg['user_id'])
-
-
-class BSFilterFuncKeyEvent(ResourceConfigEvent):
-    routing_key = 'config.bsfilter.{}'
-
-    def __init__(self, func_key_id, filter_id, secretary_id):
-        self.func_key_id = func_key_id
-        self.filter_id = filter_id
-        self.secretary_id = secretary_id
-
-    def marshal(self):
-        return {'id': self.func_key_id,
-                'destination': 'bsfilter',
-                'filter_id': self.filter_id,
-                'secretary_id': self.secretary_id}
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(msg['id'], msg['filter_id'], msg['secretary_id'])
-
-
-class UserCreateFuncKeyEvent(UserFuncKeyEvent):
-    name = 'func_key_created'
-    routing_key = UserFuncKeyEvent.routing_key.format('created')
-
-
-class UserDeleteFuncKeyEvent(UserFuncKeyEvent):
-    name = 'func_key_deleted'
-    routing_key = UserFuncKeyEvent.routing_key.format('deleted')
-
-
-class BSFilterCreateFuncKeyEvent(BSFilterFuncKeyEvent):
-    name = 'func_key_created'
-    routing_key = BSFilterFuncKeyEvent.routing_key.format('created')
-
-
-class BSFilterDeleteFuncKeyEvent(BSFilterFuncKeyEvent):
-    name = 'func_key_deleted'
-    routing_key = BSFilterFuncKeyEvent.routing_key.format('deleted')
