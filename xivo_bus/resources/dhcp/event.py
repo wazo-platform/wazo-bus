8d70a5a22de9120881a418f32188d5622ecff2bb
Julien Alie <jalie@wazo.io>
2022-07-20 14:43:08 -0400

    update dhcp events

--- xivo_bus/resources/dhcp/event.py
+++ xivo_bus/resources/dhcp/event.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import ServiceEvent
 
-from xivo_bus.resources.common.event import ArbitraryEvent
 
-
-class EditDHCPEvent(ArbitraryEvent):
+class DHCPEditedEvent(ServiceEvent):
+    name = 'dhcp_edited'
+    routing_key_fmt = 'config.dhcp.edited'
 
     def __init__(self):
-        super(EditDHCPEvent, self).__init__(
-            name='dhcp_edited',
-            body={},
-            required_acl='events.config.dhcp.edited',
-        )
-        self.routing_key = 'config.dhcp.edited'
+        super(DHCPEditedEvent, self).__init__()
