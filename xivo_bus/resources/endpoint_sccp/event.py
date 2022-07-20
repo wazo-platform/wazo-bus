14315a5f168087ef7dd681082df158a5d310ba65
Julien Alie <jalie@wazo.io>
2022-07-20 15:00:44 -0400

    update endpoint_sccp events
    - renamed `updated` to `edited`

--- xivo_bus/resources/endpoint_sccp/event.py
+++ xivo_bus/resources/endpoint_sccp/event.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from ..common.event import BaseEvent
-
-
-class _BaseSccpEndpointEvent(BaseEvent):
-
-    def __init__(self, endpoint_iax):
-        self._body = endpoint_iax
-        super(_BaseSccpEndpointEvent, self).__init__()
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class EditSccpEndpointEvent(_BaseSccpEndpointEvent):
-    name = 'sccp_endpoint_updated'
-    routing_key_fmt = 'config.sccp_endpoint.updated'
-
-
-class CreateSccpEndpointEvent(_BaseSccpEndpointEvent):
+class SCCPEndpointCreatedEvent(TenantEvent):
     name = 'sccp_endpoint_created'
     routing_key_fmt = 'config.sccp_endpoint.created'
 
+    def __init__(self, endpoint_sccp, tenant_uuid):
+        super(SCCPEndpointCreatedEvent, self).__init__(endpoint_sccp, tenant_uuid)
 
-class DeleteSccpEndpointEvent(_BaseSccpEndpointEvent):
+
+class SCCPEndpointDeletedEvent(TenantEvent):
     name = 'sccp_endpoint_deleted'
     routing_key_fmt = 'config.sccp_endpoint.deleted'
+
+    def __init__(self, endpoint_sccp, tenant_uuid):
+        super(SCCPEndpointDeletedEvent, self).__init__(endpoint_sccp, tenant_uuid)
+
+
+class SCCPEndpointEditedEvent(TenantEvent):
+    name = 'sccp_endpoint_edited'
+    routing_key_fmt = 'config.sccp_endpoint.edited'
+
+    def __init__(self, endpoint_sccp, tenant_uuid):
+        super(SCCPEndpointEditedEvent, self).__init__(endpoint_sccp, tenant_uuid)
