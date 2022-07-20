cde9f22b1ba48e368aa31906d34c4c3d83c228cf
Julien Alie <jalie@wazo.io>
2022-07-20 16:24:25 -0400

    update HA events
    - Removed ArbitraryEvent

--- xivo_bus/resources/ha/event.py
+++ xivo_bus/resources/ha/event.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import ServiceEvent
 
-from xivo_bus.resources.common.event import ArbitraryEvent
 
-
-class EditHAEvent(ArbitraryEvent):
+class HAEditedEvent(ServiceEvent):
+    name = 'ha_edited'
+    routing_key_fmt = 'config.ha.edited'
 
     def __init__(self):
-        super(EditHAEvent, self).__init__(
-            name='ha_edited',
-            body={},
-            required_acl='events.config.ha.edited',
-        )
-        self.routing_key = 'config.ha.edited'
+        super(HAEditedEvent, self).__init__()
