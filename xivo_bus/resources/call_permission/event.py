3bf40920ba648c3f95fe3c22b914b098842deb58
Julien Alie <jalie@wazo.io>
2022-07-20 11:59:00 -0400

    update call_permission events

--- xivo_bus/resources/call_permission/event.py
+++ xivo_bus/resources/call_permission/event.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Avencall
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditCallPermissionEvent(ResourceConfigEvent):
-    name = 'call_permission_edited'
-    routing_key = 'config.callpermission.edited'
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class CreateCallPermissionEvent(ResourceConfigEvent):
+class CallPermissionCreatedEvent(TenantEvent):
     name = 'call_permission_created'
-    routing_key = 'config.callpermission.created'
+    routing_key_fmt = 'config.callpermission.created'
+
+    def __init__(self, call_permission_id, tenant_uuid):
+        content = {'id': call_permission_id}
+        super(CallPermissionCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteCallPermissionEvent(ResourceConfigEvent):
+class CallPermissionDeletedEvent(TenantEvent):
     name = 'call_permission_deleted'
-    routing_key = 'config.callpermission.deleted'
+    routing_key_fmt = 'config.callpermission.deleted'
+
+    def __init__(self, call_permission_id, tenant_uuid):
+        content = {'id': call_permission_id}
+        super(CallPermissionCreatedEvent, self).__init__(content, tenant_uuid)
+
+
+class CallPermissionEditedEvent(TenantEvent):
+    name = 'call_permission_edited'
+    routing_key_fmt = 'config.callpermission.edited'
+
+    def __init__(self, call_permission_id, tenant_uuid):
+        content = {'id': call_permission_id}
+        super(CallPermissionCreatedEvent, self).__init__(content, tenant_uuid)
