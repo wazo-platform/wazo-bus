4c73db66f35e4cc56571a8f623b63d46591f92c9
Julien Alie <jalie@wazo.io>
2022-07-21 14:18:12 -0400

    update skill_rule events

--- xivo_bus/resources/skill_rule/event.py
+++ xivo_bus/resources/skill_rule/event.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditSkillRuleEvent(ResourceConfigEvent):
-    name = 'skill_rule_edited'
-    routing_key = 'config.queues.skillrules.edited'
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class CreateSkillRuleEvent(ResourceConfigEvent):
+class SkillRuleCreatedEvent(TenantEvent):
     name = 'skill_rule_created'
-    routing_key = 'config.queues.skillrules.created'
+    routing_key_fmt = 'config.queues.skillrules.created'
+
+    def __init__(self, skill_rule_id, tenant_uuid):
+        content = {'id': int(skill_rule_id)}
+        super(SkillRuleCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteSkillRuleEvent(ResourceConfigEvent):
+class SkillRuleDeletedEvent(TenantEvent):
     name = 'skill_rule_deleted'
-    routing_key = 'config.queues.skillrules.deleted'
+    routing_key_fmt = 'config.queues.skillrules.deleted'
+
+    def __init__(self, skill_rule_id, tenant_uuid):
+        content = {'id': int(skill_rule_id)}
+        super(SkillRuleDeletedEvent, self).__init__(content, tenant_uuid)
+
+
+class SkillRuleEditedEvent(TenantEvent):
+    name = 'skill_rule_edited'
+    routing_key_fmt = 'config.queues.skillrules.edited'
+
+    def __init__(self, skill_rule_id, tenant_uuid):
+        content = {'id': int(skill_rule_id)}
+        super(SkillRuleEditedEvent, self).__init__(content, tenant_uuid)
