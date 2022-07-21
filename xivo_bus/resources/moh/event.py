4a550d1594e2ea18ad2dc887395e6d5a03da1cac
Julien Alie <jalie@wazo.io>
2022-07-21 10:27:48 -0400

    update moh events

--- xivo_bus/resources/moh/event.py
+++ xivo_bus/resources/moh/event.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from ..common.event import BaseEvent
-
-
-class _BaseMohEvent(BaseEvent):
-
-    def __init__(self, moh):
-        self._body = moh
-        super(_BaseMohEvent, self).__init__()
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class EditMohEvent(_BaseMohEvent):
-    name = 'moh_edited'
-    routing_key_fmt = 'config.moh.edited'
-
-
-class CreateMohEvent(_BaseMohEvent):
+class MOHCreatedEvent(TenantEvent):
     name = 'moh_created'
     routing_key_fmt = 'config.moh.created'
 
+    def __init__(self, moh, tenant_uuid):
+        super(MOHCreatedEvent, self).__init__(moh, tenant_uuid)
 
-class DeleteMohEvent(_BaseMohEvent):
+
+class MOHDeletedEvent(TenantEvent):
     name = 'moh_deleted'
     routing_key_fmt = 'config.moh.deleted'
+
+    def __init__(self, moh, tenant_uuid):
+        super(MOHDeletedEvent, self).__init__(moh, tenant_uuid)
+
+
+class MOHEditedEvent(TenantEvent):
+    name = 'moh_edited'
+    routing_key_fmt = 'config.moh.edited'
+
+    def __init__(self, moh, tenant_uuid):
+        super(MOHEditedEvent, self).__init__(moh, tenant_uuid)
