0f233b69689f95893c693aa1aba5b98295084b06
Julien Alie <jalie@wazo.io>
2022-07-21 14:03:35 -0400

    update rtp events

--- xivo_bus/resources/rtp/event.py
+++ xivo_bus/resources/rtp/event.py
@@ -1,30 +1,22 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import ServiceEvent
 
 
-class _RTPConfigurationEvent(object):
-    def marshal(self):
-        return {}
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls()
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def __ne__(self, other):
-        return not self == other
-
-
-class EditRTPGeneralEvent(_RTPConfigurationEvent):
+class RTPGeneralEditedEvent(ServiceEvent):
     name = 'rtp_general_edited'
-    routing_key = 'config.rtp_general.edited'
+    routing_key_fmt = 'config.rtp_general.edited'
+
+    def __init__(self):
+        super(RTPGeneralEditedEvent, self).__init__()
 
 
-class EditRTPIceHostCandidatesEvent(_RTPConfigurationEvent):
+class RTPIceHostCandidatesEditedEvent(ServiceEvent):
     name = 'rtp_ice_host_candidates_edited'
-    routing_key = 'config.rtp_ice_host_candidates.edited'
+    routing_key_fmt = 'config.rtp_ice_host_candidates.edited'
+
+    def __init__(self):
+        super(RTPIceHostCandidatesEditedEvent, self).__init__()
