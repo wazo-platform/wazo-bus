ca0efda3b1f8fedcf593f3854082a0df5de94468
Julien Alie <jalie@wazo.io>
2022-07-21 17:09:33 -0400

    update agent events

--- xivo_bus/resources/agent/event.py
+++ xivo_bus/resources/agent/event.py
@@ -3,23 +3,34 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent, BaseEvent
-
-
-class EditAgentEvent(ResourceConfigEvent):
-    name = 'agent_edited'
-    routing_key = 'config.agent.edited'
+from xivo_bus.resources.common.event import TenantEvent, BaseEvent
 
 
-class CreateAgentEvent(ResourceConfigEvent):
+class AgentCreatedEvent(TenantEvent):
     name = 'agent_created'
-    routing_key = 'config.agent.created'
+    routing_key_fmt = 'config.agent.created'
+
+    def __init__(self, agent_id, tenant_uuid):
+        content = {'id': int(agent_id)}
+        super(AgentCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteAgentEvent(ResourceConfigEvent):
+class AgentDeletedEvent(TenantEvent):
     name = 'agent_deleted'
-    routing_key = 'config.agent.deleted'
+    routing_key_fmt = 'config.agent.deleted'
+
+    def __init__(self, agent_id, tenant_uuid):
+        content = {'id': int(agent_id)}
+        super(AgentDeletedEvent, self).__init__(content, tenant_uuid)
+
+
+class AgentEditedEvent(TenantEvent):
+    name = 'agent_edited'
+    routing_key_fmt = 'config.agent.edited'
+
+    def __init__(self, agent_id, tenant_uuid):
+        content = {'id': int(agent_id)}
+        super(AgentEditedEvent, self).__init__(content, tenant_uuid)
 
 
 class PauseAgentEvent(BaseEvent):
