e8840409129a21e4e1474814ba9667094ea3639a
Julien Alie <jalie@wazo.io>
2022-07-21 11:15:46 -0400

    update pjsip events

--- xivo_bus/resources/pjsip/event.py
+++ xivo_bus/resources/pjsip/event.py
@@ -1,49 +1,46 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import ServiceEvent
 
-from ..common.event import BaseEvent
 
-
-class PJSIPGlobalUpdatedEvent(BaseEvent):
-
+class PJSIPGlobalUpdatedEvent(ServiceEvent):
     name = 'pjsip_global_updated'
     routing_key_fmt = 'config.pjsip_global.updated'
 
     def __init__(self):
-        self._body = {}
         super(PJSIPGlobalUpdatedEvent, self).__init__()
 
 
-class PJSIPSystemUpdatedEvent(BaseEvent):
-
+class PJSIPSystemUpdatedEvent(ServiceEvent):
     name = 'pjsip_system_updated'
     routing_key_fmt = 'config.pjsip_system.updated'
 
     def __init__(self):
-        self._body = {}
         super(PJSIPSystemUpdatedEvent, self).__init__()
 
 
-class _BasePJSIPTransportEvent(BaseEvent):
-
-    def __init__(self, transport):
-        self._body = transport
-        super(_BasePJSIPTransportEvent, self).__init__()
-
-
-class EditSIPTransportEvent(_BasePJSIPTransportEvent):
-    name = 'sip_transport_edited'
-    routing_key_fmt = 'config.sip.transports.edited'
-
-
-class CreateSIPTransportEvent(_BasePJSIPTransportEvent):
+class SIPTransportCreatedEvent(ServiceEvent):
     name = 'sip_transport_created'
     routing_key_fmt = 'config.sip.transports.created'
 
+    def __init__(self, transport):
+        super(SIPTransportCreatedEvent, self).__init__(transport)
 
-class DeleteSIPTransportEvent(_BasePJSIPTransportEvent):
+
+class SIPTransportDeletedEvent(ServiceEvent):
     name = 'sip_transport_deleted'
     routing_key_fmt = 'config.sip.transports.deleted'
+
+    def __init__(self, transport):
+        super(SIPTransportDeletedEvent, self).__init__(transport)
+
+
+class SIPTransportEditedEvent(ServiceEvent):
+    name = 'sip_transport_edited'
+    routing_key_fmt = 'config.sip.transports.edited'
+
+    def __init__(self, transport):
+        super(SIPTransportEditedEvent, self).__init__(transport)
