302195e5a2aee6a5cb63082b7e66151ce182999d
Julien Alie <jalie@wazo.io>
2022-07-21 14:15:03 -0400

    update skills events

--- xivo_bus/resources/skill/event.py
+++ xivo_bus/resources/skill/event.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditSkillEvent(ResourceConfigEvent):
-    name = 'skill_edited'
-    routing_key = 'config.agents.skills.edited'
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class CreateSkillEvent(ResourceConfigEvent):
+class SkillCreatedEvent(TenantEvent):
     name = 'skill_created'
-    routing_key = 'config.agents.skills.created'
+    routing_key_fmt = 'config.agents.skills.created'
+
+    def __init__(self, skill_id, tenant_uuid):
+        content = {'id': int(skill_id)}
+        super(SkillCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteSkillEvent(ResourceConfigEvent):
+class SkillDeletedEvent(TenantEvent):
     name = 'skill_deleted'
-    routing_key = 'config.agents.skills.deleted'
+    routing_key_fmt = 'config.agents.skills.deleted'
+
+    def __init__(self, skill_id, tenant_uuid):
+        content = {'id': int(skill_id)}
+        super(SkillDeletedEvent, self).__init__(content, tenant_uuid)
+
+
+class SkillEditedEvent(TenantEvent):
+    name = 'skill_edited'
+    routing_key_fmt = 'config.agents.skills.edited'
+
+    def __init__(self, skill_id, tenant_uuid):
+        content = {'id': int(skill_id)}
+        super(SkillEditedEvent, self).__init__(content, tenant_uuid)
