e170221de5909971804b45e15e627a3b83476fc0
Julien Alie <jalie@wazo.io>
2022-07-21 16:10:17 -0400

    update user_line_extension events

--- xivo_bus/resources/user_line_extension/event.py
+++ xivo_bus/resources/user_line_extension/event.py
@@ -1,55 +1,46 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Avencall
+# Copyright 2013-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import UserEvent
 
-from xivo_bus.resources.common.event import ResourceConfigEvent
 
-
-class UserLineExtensionConfigEvent(ResourceConfigEvent):
-
-    def __init__(self,
-                 user_line_extension_id,
-                 user_id,
-                 line_id,
-                 extension_id,
-                 main_user,
-                 main_line):
-        self.id = int(user_line_extension_id)
-        self.user_id = int(user_id)
-        self.line_id = int(line_id)
-        self.extension_id = int(extension_id)
-        self.main_user = bool(main_user)
-        self.main_line = bool(main_line)
-
-    def marshal(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'line_id': self.line_id,
-            'extension_id': self.extension_id,
-            'main_user': self.main_user,
-            'main_line': self.main_line
+class _BaseUserLineExtensionEvent(UserEvent):
+    def __init__(
+        self,
+        user_line_extension_id,
+        user_id,
+        line_id,
+        extension_id,
+        main_user,
+        main_line,
+        tenant_uuid,
+        user_uuid,
+    ):
+        content = {
+            'id': int(user_line_extension_id),
+            'user_id': int(user_id),
+            'line_id': int(line_id),
+            'extension_id': int(extension_id),
+            'main_user': bool(main_user),
+            'main_line': bool(main_line),
         }
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(msg['id'],
-                   msg['user_id'],
-                   msg['line_id'],
-                   msg['extension_id'],
-                   msg['main_user'],
-                   msg['main_line'])
+        super(_BaseUserLineExtensionEvent, self).__init__(
+            content, tenant_uuid, user_uuid
+        )
 
 
-class CreateUserLineExtensionEvent(UserLineExtensionConfigEvent):
+class UserLineExtensionCreatedEvent(_BaseUserLineExtensionEvent):
     name = 'user_line_extension_created'
+    routing_key_fmt = 'config.users.lines.extensions.created'
 
 
-class EditUserLineExtensionEvent(UserLineExtensionConfigEvent):
-    name = 'user_line_extension_edited'
-
-
-class DeleteUserLineExtensionEvent(UserLineExtensionConfigEvent):
+class UserLineExtensionDeletedEvent(_BaseUserLineExtensionEvent):
     name = 'user_line_extension_deleted'
+    routing_key_fmt = 'config.users.lines.extensions.deleted'
+
+
+class UserLineExtensionEditedEvent(_BaseUserLineExtensionEvent):
+    name = 'user_line_extension_edited'
+    routing_key_fmt = 'config.users.lines.extensions.edited'
