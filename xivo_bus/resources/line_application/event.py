2153435c0e16338afaf49b26ae80c4166eda46e1
Julien Alie <jalie@wazo.io>
2022-07-21 08:38:38 -0400

    update line_application events

--- xivo_bus/resources/line_application/event.py
+++ xivo_bus/resources/line_application/event.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from ..common.event import BaseEvent
-
-
-class _BaseLineApplicationEvent(BaseEvent):
-
-    def __init__(self, line, application):
-        self._body = {'line': line, 'application': application}
-        super(_BaseLineApplicationEvent, self).__init__()
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class LineApplicationAssociatedEvent(_BaseLineApplicationEvent):
-
+class LineApplicationAssociatedEvent(TenantEvent):
     name = 'line_application_associated'
     routing_key_fmt = 'config.lines.{line[id]}.applications.{application[uuid]}.updated'
 
+    def __init__(self, line, application, tenant_uuid):
+        content = {'line': line, 'application': application}
+        super(LineApplicationAssociatedEvent, self).__init__(content, tenant_uuid)
 
-class LineApplicationDissociatedEvent(_BaseLineApplicationEvent):
 
+class LineApplicationDissociatedEvent(TenantEvent):
     name = 'line_application_dissociated'
-    routing_key_fmt = 'config.lines.{line[id]}.applications.{application[uuid]}.deleted'
+    routing_key_fmt = 'config.lines.{line[id]}.applications.{application[uuid]}.updated'
+
+    def __init__(self, line, application, tenant_uuid):
+        content = {'line': line, 'application': application}
+        super(LineApplicationDissociatedEvent, self).__init__(content, tenant_uuid)
