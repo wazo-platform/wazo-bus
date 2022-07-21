c388d768192ce4ced0bca67cbf5481119eb12b4a
Julien Alie <jalie@wazo.io>
2022-07-21 14:59:10 -0400

    update trunk_register events

--- xivo_bus/resources/trunk_register/event.py
+++ xivo_bus/resources/trunk_register/event.py
@@ -1,41 +1,27 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class TrunkRegisterConfigEvent(object):
-
-    def __init__(self, trunk_id, register_id):
-        self.trunk_id = trunk_id
-        self.register_id = register_id
-
-    def marshal(self):
-        return {
-            'trunk_id': self.trunk_id,
-            'register_id': self.register_id,
-        }
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(
-            msg['trunk_id'],
-            msg['register_id'])
-
-    def __eq__(self, other):
-        return (self.trunk_id == other.trunk_id
-                and self.register_id == other.register_id)
-
-    def __ne__(self, other):
-        return not self == other
-
-
-class TrunkRegisterIAXAssociatedEvent(TrunkRegisterConfigEvent):
+class TrunkRegisterIAXAssociatedEvent(TenantEvent):
     name = 'trunk_register_iax_associated'
-    routing_key = 'config.trunks.registers.iax.updated'
+    routing_key_fmt = 'config.trunks.registers.iax.updated'
+
+    def __init__(self, trunk_id, register_id, tenant_uuid):
+        content = {'trunk_id': trunk_id, 'register_id': register_id}
+        super(TrunkRegisterIAXAssociatedEvent, self).__init__(content, tenant_uuid)
 
 
-class TrunkRegisterIAXDissociatedEvent(TrunkRegisterConfigEvent):
+class TrunkRegisterIAXDissociatedEvent(TenantEvent):
     name = 'trunk_register_iax_dissociated'
-    routing_key = 'config.trunks.registers.iax.deleted'
+    routing_key_fmt = 'config.trunks.registers.iax.deleted'
+
+    def __init__(self, trunk_id, register_id, tenant_uuid):
+        content = {
+            'trunk_id': trunk_id,
+            'register_id': register_id,
+        }
+        super(TrunkRegisterIAXDissociatedEvent, self).__init__(content, tenant_uuid)
