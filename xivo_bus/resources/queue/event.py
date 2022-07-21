04f4f83278e8dcb7ee9fee698c2193e7774d5b0d
Julien Alie <jalie@wazo.io>
2022-07-21 11:23:44 -0400

    update queue events

--- xivo_bus/resources/queue/event.py
+++ xivo_bus/resources/queue/event.py
@@ -1,27 +1,42 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditQueueEvent(ResourceConfigEvent):
-    name = 'queue_edited'
-    routing_key = 'config.queue.edited'
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class CreateQueueEvent(ResourceConfigEvent):
+class QueueCreatedEvent(TenantEvent):
     name = 'queue_created'
-    routing_key = 'config.queue.created'
+    routing_key_fmt = 'config.queues.created'
+
+    def __init__(self, queue_id, tenant_uuid):
+        content = {'id': int(queue_id)}
+        super(QueueCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteQueueEvent(ResourceConfigEvent):
+class QueueDeletedEvent(TenantEvent):
     name = 'queue_deleted'
-    routing_key = 'config.queue.deleted'
+    routing_key_fmt = 'config.queues.deleted'
+
+    def __init__(self, queue_id, tenant_uuid):
+        content = {'id': int(queue_id)}
+        super(QueueDeletedEvent, self).__init__(content, tenant_uuid)
 
 
-class EditQueueFallbackEvent(ResourceConfigEvent):
+class QueueEditedEvent(TenantEvent):
+    name = 'queue_edited'
+    routing_key_fmt = 'config.queues.edited'
+
+    def __init__(self, queue_id, tenant_uuid):
+        content = {'id': int(queue_id)}
+        super(QueueEditedEvent, self).__init__(content, tenant_uuid)
+
+
+class QueueFallbackEditedEvent(TenantEvent):
     name = 'queue_fallback_edited'
-    routing_key = 'config.queues.fallbacks.edited'
+    routing_key_fmt = 'config.queues.fallbacks.edited'
+
+    def __init__(self, queue_id, tenant_uuid):
+        content = {'id': int(queue_id)}
+        super(QueueFallbackEditedEvent, self).__init__(content, tenant_uuid)
