3f1557e145eebda87b9e2a640fe8ec8b913f41a9
Julien Alie <jalie@wazo.io>
2022-07-21 08:41:01 -0400

    update line_device events

--- xivo_bus/resources/line_device/event.py
+++ xivo_bus/resources/line_device/event.py
@@ -1,29 +1,24 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from ..common.event import BaseEvent
-
-
-class _BaseLineDeviceEvent(BaseEvent):
-
-    def __init__(self, line, device):
-        self._body = {
-            'line': line,
-            'device': device,
-        }
-        super(_BaseLineDeviceEvent, self).__init__()
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class LineDeviceAssociatedEvent(_BaseLineDeviceEvent):
-
+class LineDeviceAssociatedEvent(TenantEvent):
     name = 'line_device_associated'
     routing_key_fmt = 'config.lines.{line[id]}.devices.{device[id]}.updated'
 
+    def __init__(self, line, device, tenant_uuid):
+        content = {'line': line, 'device': device}
+        super(LineDeviceAssociatedEvent, self).__init__(content, tenant_uuid)
 
-class LineDeviceDissociatedEvent(_BaseLineDeviceEvent):
 
+class LineDeviceDissociatedEvent(TenantEvent):
     name = 'line_device_dissociated'
     routing_key_fmt = 'config.lines.{line[id]}.devices.{device[id]}.deleted'
+
+    def __init__(self, line, device, tenant_uuid):
+        content = {'line': line, 'device': device}
+        super(LineDeviceDissociatedEvent, self).__init__(content, tenant_uuid)
