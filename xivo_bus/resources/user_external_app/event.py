cbd615d62ed52eefcd126d0e6667d7ac30ad861d
Julien Alie <jalie@wazo.io>
2022-07-21 15:55:41 -0400

    update user_external_app events

--- xivo_bus/resources/user_external_app/event.py
+++ xivo_bus/resources/user_external_app/event.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import BaseEvent
-
-
-class _BaseUserExternalAppEvent(BaseEvent):
-
-    def __init__(self, app):
-        self._body = app
-        super(_BaseUserExternalAppEvent, self).__init__()
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class EditUserExternalAppEvent(_BaseUserExternalAppEvent):
-    name = 'user_external_app_edited'
-    routing_key_fmt = 'config.user_external_apps.edited'
-
-
-class CreateUserExternalAppEvent(_BaseUserExternalAppEvent):
+class UserExternalAppCreatedEvent(TenantEvent):
     name = 'user_external_app_created'
     routing_key_fmt = 'config.user_external_apps.created'
 
+    def __init__(self, app, tenant_uuid):
+        super(UserExternalAppCreatedEvent, self).__init__(app, tenant_uuid)
 
-class DeleteUserExternalAppEvent(_BaseUserExternalAppEvent):
-    name = 'user_external_app_deleted'
+
+class UserExternalAppDeletedEvent(TenantEvent):
+    name = 'user_external_app_created'
     routing_key_fmt = 'config.user_external_apps.deleted'
+
+    def __init__(self, app, tenant_uuid):
+        super(UserExternalAppDeletedEvent, self).__init__(app, tenant_uuid)
+
+
+class UserExternalAppEditedEvent(TenantEvent):
+    name = 'user_external_app_edited'
+    routing_key_fmt = 'config.user_external_apps.edited'
+
+    def __init__(self, app, tenant_uuid):
+        super(UserExternalAppEditedEvent, self).__init__(app, tenant_uuid)
