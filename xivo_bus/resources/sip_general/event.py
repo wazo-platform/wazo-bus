2ae4164f704135018e0242210c246d8e132c66d5
Julien Alie <jalie@wazo.io>
2022-07-21 14:11:41 -0400

    update sip_general events

--- xivo_bus/resources/sip_general/event.py
+++ xivo_bus/resources/sip_general/event.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Avencall
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import ServiceEvent
 
 
-class EditSIPGeneralEvent(object):
+class SIPGeneralEditedEvent(ServiceEvent):
     name = 'sip_general_edited'
-    routing_key = 'config.sip_general.edited'
+    routing_key_fmt = 'config.sip_general.edited'
 
-    def marshal(self):
-        return {}
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls()
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def __ne__(self, other):
-        return not self == other
+    def __init__(self):
+        super(SIPGeneralEditedEvent, self).__init__()
