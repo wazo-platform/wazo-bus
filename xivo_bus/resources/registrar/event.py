22fb7b0cf89d26c94e294e93c375657f519514bf
Julien Alie <jalie@wazo.io>
2022-07-21 13:59:51 -0400

    update registrar events

--- xivo_bus/resources/registrar/event.py
+++ xivo_bus/resources/registrar/event.py
@@ -1,34 +1,30 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import BaseEvent
-
-
-class DeleteRegistrarEvent(BaseEvent):
-    name = 'registrar_deleted'
-    routing_key_fmt = 'config.registrar.deleted'
-
-    def __init__(self, registrar_info):
-        self._body = registrar_info
-        super(DeleteRegistrarEvent, self).__init__()
+from xivo_bus.resources.common.event import ServiceEvent
 
 
-class EditRegistrarEvent(BaseEvent):
-    name = 'registrar_edited'
-    routing_key_fmt = 'config.registrar.edited'
-
-    def __init__(self, registrar_info):
-        self._body = registrar_info
-        super(EditRegistrarEvent, self).__init__()
-
-
-class CreateRegistrarEvent(BaseEvent):
+class RegistrarCreatedEvent(ServiceEvent):
     name = 'registrar_created'
     routing_key_fmt = 'config.registrar.created'
 
-    def __init__(self, registrar_info):
-        self._body = registrar_info
-        super(CreateRegistrarEvent, self).__init__()
+    def __init__(self, registrar):
+        super(RegistrarCreatedEvent, self).__init__(registrar)
+
+
+class RegistrarDeletedEvent(ServiceEvent):
+    name = 'registrar_deleted'
+    routing_key_fmt = 'config.registrar.deleted'
+
+    def __init__(self, registrar):
+        super(RegistrarDeletedEvent, self).__init__(registrar)
+
+
+class RegistrarEditedEvent(ServiceEvent):
+    name = 'registrar_edited'
+    routing_key_fmt = 'config.registrar.edited'
+
+    def __init__(self, registrar):
+        super(RegistrarEditedEvent, self).__init__(registrar)
