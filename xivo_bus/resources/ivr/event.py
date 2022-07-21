91800e8017d05340e2c238ba0dda6019813e2c3f
Julien Alie <jalie@wazo.io>
2022-07-21 08:29:47 -0400

    update IVR events

--- xivo_bus/resources/ivr/event.py
+++ xivo_bus/resources/ivr/event.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Proformatique Inc.
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditIvrEvent(ResourceConfigEvent):
-    name = 'ivr_edited'
-    routing_key = 'config.ivr.edited'
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class CreateIvrEvent(ResourceConfigEvent):
+class IVRCreatedEvent(TenantEvent):
     name = 'ivr_created'
-    routing_key = 'config.ivr.created'
+    routing_key_fmt = 'config.ivr.created'
+
+    def __init__(self, ivr_id, tenant_uuid):
+        content = {'id': ivr_id}
+        super(IVRCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteIvrEvent(ResourceConfigEvent):
+class IVRDeletedEvent(TenantEvent):
     name = 'ivr_deleted'
-    routing_key = 'config.ivr.deleted'
+    routing_key_fmt = 'config.ivr.deleted'
+
+    def __init__(self, ivr_id, tenant_uuid):
+        content = {'id': ivr_id}
+        super(IVRDeletedEvent, self).__init__(content, tenant_uuid)
+
+
+class IVREditedEvent(TenantEvent):
+    name = 'ivr_edited'
+    routing_key_fmt = 'config.ivr.edited'
+
+    def __init__(self, ivr_id, tenant_uuid):
+        content = {'id': ivr_id}
+        super(IVREditedEvent, self).__init__(content, tenant_uuid)
