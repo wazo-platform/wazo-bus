3f0ada5cda103aacd2ef3853523c870059db3226
Julien Alie <jalie@wazo.io>
2022-07-21 13:48:57 -0400

    update register events

--- xivo_bus/resources/register/event.py
+++ xivo_bus/resources/register/event.py
@@ -1,37 +1,60 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditRegisterSIPEvent(ResourceConfigEvent):
-    name = 'register_sip_edited'
-    routing_key = 'config.register.sip.edited'
+from xivo_bus.resources.common.event import ServiceEvent
 
 
-class CreateRegisterSIPEvent(ResourceConfigEvent):
+class RegisterSIPCreated(ServiceEvent):
     name = 'register_sip_created'
-    routing_key = 'config.register.sip.created'
+    routing_key_fmt = 'config.register.sip.created'
+
+    def __init__(self, register_id):
+        content = {'id': int(register_id)}
+        super(RegisterSIPCreated, self).__init__(content)
 
 
-class DeleteRegisterSIPEvent(ResourceConfigEvent):
+class RegisterSIPDeleted(ServiceEvent):
     name = 'register_sip_deleted'
-    routing_key = 'config.register.sip.deleted'
+    routing_key_fmt = 'config.register.sip.deleted'
+
+    def __init__(self, register_id):
+        content = {'id': int(register_id)}
+        super(RegisterSIPDeleted, self).__init__(content)
 
 
-class EditRegisterIAXEvent(ResourceConfigEvent):
-    name = 'register_iax_edited'
-    routing_key = 'config.register.iax.edited'
+class RegisterSIPEditedEvent(ServiceEvent):
+    name = 'register_sip_edited'
+    routing_key_fmt = 'config.register.sip.edited'
+
+    def __init__(self, register_id):
+        content = {'id': int(register_id)}
+        super(RegisterSIPEditedEvent, self).__init__(content)
 
 
-class CreateRegisterIAXEvent(ResourceConfigEvent):
+class RegisterIAXCreatedEvent(ServiceEvent):
     name = 'register_iax_created'
-    routing_key = 'config.register.iax.created'
+    routing_key_fmt = 'config.register.iax.created'
+
+    def __init__(self, register_id):
+        content = {'id': int(register_id)}
+        super(RegisterIAXCreatedEvent, self).__init__(content)
 
 
-class DeleteRegisterIAXEvent(ResourceConfigEvent):
+class RegisterIAXDeletedEvent(ServiceEvent):
     name = 'register_iax_deleted'
-    routing_key = 'config.register.iax.deleted'
+    routing_key_fmt = 'config.register.iax.deleted'
+
+    def __init__(self, register_id):
+        content = {'id': int(register_id)}
+        super(RegisterIAXDeletedEvent, self).__init__(content)
+
+
+class RegisterIAXEditedEvent(ServiceEvent):
+    name = 'register_iax_edited'
+    routing_key_fmt = 'config.register.iax.edited'
+
+    def __init__(self, register_id):
+        content = {'id': int(register_id)}
+        super(RegisterIAXEditedEvent, self).__init__(content)
