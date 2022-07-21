c9f7b51725b0ba56d73b47d0cb1a350738127dcf
Julien Alie <jalie@wazo.io>
2022-07-21 10:31:31 -0400

    update outcall events

--- xivo_bus/resources/outcall/event.py
+++ xivo_bus/resources/outcall/event.py
@@ -1,22 +1,33 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Avencall
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
-
-from xivo_bus.resources.common.event import ResourceConfigEvent
-
-
-class EditOutcallEvent(ResourceConfigEvent):
-    name = 'outcall_edited'
-    routing_key = 'config.outcalls.edited'
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class CreateOutcallEvent(ResourceConfigEvent):
+class OutcallCreatedEvent(TenantEvent):
     name = 'outcall_created'
-    routing_key = 'config.outcalls.created'
+    routing_key_fmt = 'config.outcalls.created'
+
+    def __init__(self, outcall_id, tenant_uuid):
+        content = {'id': outcall_id}
+        super(OutcallCreatedEvent, self).__init__(content, tenant_uuid)
 
 
-class DeleteOutcallEvent(ResourceConfigEvent):
+class OutcallDeletedEvent(TenantEvent):
     name = 'outcall_deleted'
-    routing_key = 'config.outcalls.deleted'
+    routing_key_fmt = 'config.outcalls.deleted'
+
+    def __init__(self, outcall_id, tenant_uuid):
+        content = {'id': outcall_id}
+        super(OutcallDeletedEvent, self).__init__(content, tenant_uuid)
+
+
+class OutcallEditedEvent(TenantEvent):
+    name = 'outcall_edited'
+    routing_key_fmt = 'config.outcalls.edited'
+
+    def __init__(self, outcall_id, tenant_uuid):
+        content = {'id': outcall_id}
+        super(OutcallEditedEvent, self).__init__(content, tenant_uuid)
