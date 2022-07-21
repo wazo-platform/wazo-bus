b5852c5a84918369dfdcd281651824b01a07fc10
Julien Alie <jalie@wazo.io>
2022-07-21 11:04:27 -0400

    update parking_lot_extension events

--- xivo_bus/resources/parking_lot_extension/event.py
+++ xivo_bus/resources/parking_lot_extension/event.py
@@ -1,41 +1,30 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import TenantEvent
 
 
-class ParkingLotExtensionConfigEvent(object):
-
-    def __init__(self, parking_lot_id, extension_id):
-        self.parking_lot_id = parking_lot_id
-        self.extension_id = extension_id
-
-    def marshal(self):
-        return {
-            'parking_lot_id': self.parking_lot_id,
-            'extension_id': self.extension_id,
-        }
-
-    @classmethod
-    def unmarshal(cls, msg):
-        return cls(
-            msg['parking_lot_id'],
-            msg['extension_id'])
-
-    def __eq__(self, other):
-        return (self.parking_lot_id == other.parking_lot_id
-                and self.extension_id == other.extension_id)
-
-    def __ne__(self, other):
-        return not self == other
-
-
-class ParkingLotExtensionAssociatedEvent(ParkingLotExtensionConfigEvent):
+class ParkingLotExtensionAssociatedEvent(TenantEvent):
     name = 'parking_lot_extension_associated'
-    routing_key = 'config.parkinglots.extensions.updated'
+    routing_key_fmt = 'config.parkinglots.extensions.updated'
+
+    def __init__(self, parking_id, extension_id, tenant_uuid):
+        content = {
+            'parking_lot_id': parking_id,
+            'extension_id': extension_id,
+        }
+        super(ParkingLotExtensionAssociatedEvent, self).__init__(content, tenant_uuid)
 
 
-class ParkingLotExtensionDissociatedEvent(ParkingLotExtensionConfigEvent):
+class ParkingLotExtensionDissociatedEvent(TenantEvent):
     name = 'parking_lot_extension_dissociated'
-    routing_key = 'config.parkinglots.extensions.deleted'
+    routing_key_fmt = 'config.parkinglots.extensions.deleted'
+
+    def __init__(self, parking_id, extension_id, tenant_uuid):
+        content = {
+            'parking_lot_id': parking_id,
+            'extension_id': extension_id,
+        }
+        super(ParkingLotExtensionDissociatedEvent, self).__init__(content, tenant_uuid)
