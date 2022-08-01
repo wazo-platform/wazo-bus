14ebaf7e9a38ac5dfa8348c21b2f9d12e3df5df4
Julien Alie <jalie@wazo.io>
2022-08-01 11:47:21 -0400

    update schema creation

--- xivo_bus/documentation.py
+++ xivo_bus/documentation.py
@@ -1,6 +1,7 @@
 import importlib
 import inspect
 import os
+import re
 import yaml
 
 from glob import glob
@@ -35,47 +36,110 @@ def generate_format():
         },
         'channels': {},
         'components': {
-            'messages': {},
-            'schemas': {
+            'x-headers': {
+                'origin_uuid': {
+                    'type': 'string',
+                    'format': 'uuid',
+                    'description': 'Unique identifier of the WAZO service that published this '
+                    'event',
+                },
+                'timestamp': {
+                    'type': 'string',
+                    'format': 'date-time',
+                    'description': 'date and time this event was published',
+                },
                 'tenant_uuid': {
                     'type': 'string',
                     'format': 'uuid',
                     'description': 'Resource\'s associated tenant\'s UUID',
                 },
-                'user_uuid': {
+                'user_uuid:{uuid}': {
                     'name': 'user_uuid',
                     'type': 'boolean',
                     'description': 'Targeted user\'s uuid.  The format user_uuid:<uuid> allows '
                     'multiple users to be the recipient of the event',
+                    'parameters': {
+                        'uuid': {
+                            'description': 'Concerned user\'s unique identifier',
+                            'schema': {
+                                'type': 'string',
+                                'format': 'uuid',
+                            },
+                        },
+                    },
                 },
             },
+            'schemas': {},
         },
     }
     return spec
 
 
 def generate_event_headers(cls, keys):
+    required = ['name', 'required_access', 'origin_uuid', 'timestamp']
     headers = {
         'name': {
             'type': 'string',
             'const': cls.name,
-        }
+            'description': 'Name of the event (used for routing the message)',
+        },
+        'required_access': {
+            'type': 'string',
+            'const': f'event.{cls.name}',
+            'description': 'Necessary user access required to read this event',
+        },
+        'origin_uuid': {'$ref': '#/components/x-headers/origin_uuid'},
+        'timestamp': {'$ref': '#/components/x-headers/timestamp'},
     }
 
     if 'tenant_uuid' in keys:
-        headers['tenant_uuid'] = {'$ref': '#/components/schemas/tenant_uuid'}
+        headers['tenant_uuid'] = {'$ref': '#/components/x-headers/tenant_uuid'}
+        required.append('tenant_uuid')
     if 'user_uuid' in keys:
-        headers['user_uuid:{uuid}'] = {'$ref': '#/components/schemas/user_uuid'}
-    return headers
+        headers['user_uuid:{uuid}'] = {'$ref': '#/components/x-headers/user_uuid:{uuid}'}
+        required.append('user_uuid:{uuid}')
+    return headers, required
 
 
-def generate_event_payload(cls):
-    try:
-        yml = yaml.safe_load(cls.__doc__)
-    except AttributeError:
+def generate_event_payload(cls, keys):
+    def contains(key, *parts):
+        return any(part in key for part in parts)
+
+    keys = [key for key in keys if key not in ('tenant_uuid', 'user_uuid', 'self')]
+    content = {}
+
+    for key in keys:
+        if contains(key, 'uuid'):
+            content[key] = {'type': 'string', 'format': 'uuid'}
+        elif contains(key, 'id', 'number'):
+            content[key] = {'type': 'integer'}
+        elif key.endswith('_at'):
+            content[key] = {'type': 'string', 'format': 'date-time'}
+        else:
+            content[key] = {'type': 'string'}
+
+    return {
+        'data': {
+            'type': 'object',
+            'properties': content,
+        },
+    }
+
+
+def generate_event_parameters(cls):
+    parameters = {}
+    matches = re.search(r'\{(.*?)\}', cls.name)
+    if matches is None:
         return {}
 
-    return {'data': yml['payload']}
+    for param in matches.groups() or []:
+        parameters[param] = {
+            'description': '',
+            'schema': {
+                'type': 'string',
+            },
+        }
+    return {'parameters': parameters}
 
 
 def generate_event_spec(cls):
@@ -87,8 +151,8 @@ def generate_event_spec(cls):
     except AttributeError:
         doc = {}
 
-    headers = generate_event_headers(cls, fn_args)
-    payload = generate_event_payload(cls)
+    headers, required_headers = generate_event_headers(cls, fn_args)
+    payload = generate_event_payload(cls, fn_args)
 
     event_spec = {
         'subscribe': {
@@ -101,11 +165,15 @@ def generate_event_spec(cls):
                 'headers': {
                     'type': 'object',
                     'properties': headers,
+                    'required': required_headers,
                 },
             },
         }
     }
-    return event_spec
+
+    parameters = generate_event_parameters(cls)
+    parameters.update(event_spec)
+    return parameters
 
 
 def get_event_spec(path):
@@ -135,7 +203,7 @@ def get_event_spec(path):
 def import_events(base_cls=None):
     resources_dir = os.path.join("resources", "**", "event*.py")
 
-    paths = glob(resources_dir, recursive=False)
+    paths = sorted(glob(resources_dir, recursive=False), reverse=True)
     spec = generate_format()
 
     with ThreadPoolExecutor() as executor:
@@ -143,5 +211,8 @@ def import_events(base_cls=None):
 
     spec['channels'] = dict(ChainMap(*[result for result in results]))
 
-    with open('events_spec.yml', 'w') as file:
-        yaml.dump(spec, file, sort_keys=False)
+    try:
+        with open('events_spec.yml', 'w') as file:
+            yaml.dump(spec, file, sort_keys=False)
+    except Exception:
+        raise
