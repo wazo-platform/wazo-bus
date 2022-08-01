3b5e7de47c1c01703568610a3ce492ac54c2a3b1
Julien Alie <jalie@wazo.io>
2022-08-01 11:58:46 -0400

    reworked headers generation to avoid kwargs clash

--- xivo_bus/resources/common/abstract.py
+++ xivo_bus/resources/common/abstract.py
@@ -2,28 +2,11 @@
 # Copyright 2022-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import re
-
-from inspect import isabstract
 from abc import ABCMeta, abstractmethod
 from six import with_metaclass
 
 
-def camel_to_snake(name):
-    name = name.split('Event')[0]
-    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
-
-
-class EventMeta(ABCMeta):
-    def __new__(mcs, name, bases, attrs):
-        cls = super(EventMeta, mcs).__new__(mcs, name, bases, attrs)
-        if not isabstract(cls) and 'name' not in attrs:
-            setattr(cls, 'name', camel_to_snake(name))
-        return cls
-
-
-class AbstractEvent(with_metaclass(EventMeta, object)):
+class AbstractEvent(with_metaclass(ABCMeta, object)):
     __slots__ = ('content',)
 
     def __init__(self, content=None):
@@ -51,13 +34,18 @@ class AbstractEvent(with_metaclass(EventMeta, object)):
 
     @property
     @abstractmethod
+    def name(self):
+        pass
+
+    @property
+    @abstractmethod
     def routing_key_fmt(self):
         pass
 
     @property
     def routing_key(self):
         variables = dict(**self.content)
-        variables.update(name=self.name, **vars(self))
+        variables.update(vars(self), name=self.name)
         return self.routing_key_fmt.format(**variables)
 
     @property
@@ -67,7 +55,7 @@ class AbstractEvent(with_metaclass(EventMeta, object)):
         """
         if hasattr(self, 'required_acl_fmt'):
             variables = dict(**self.content)
-            variables.update(name=self.name, **vars(self))
+            variables.update(vars(self), name=self.name)
             return self.required_acl_fmt.format(**variables)
         return 'events.{}'.format(self.routing_key)
 
@@ -77,8 +65,9 @@ class AbstractEvent(with_metaclass(EventMeta, object)):
 
     @property
     def headers(self):
-        variables = vars(self)
-        return dict(name=self.name, **variables)
+        headers = dict(vars(self))
+        headers.update(name=self.name)
+        return headers
 
     def marshal(self):
         return self.content
