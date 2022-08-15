1a68d49408e23620d8ea5844595ed13a3e743d53
Julien Alie <jalie@wazo.io>
2022-08-15 13:24:06 -0400

    update `wazo-call-logd` events

--- xivo_bus/resources/call_logs/events.py
+++ xivo_bus/resources/call_logs/events.py
@@ -1,42 +1,23 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from xivo_bus.resources.common.event import TenantEvent, UserEvent
 
-class CallLogCreatedEvent(object):
 
+class CallLogCreatedEvent(TenantEvent):
     name = 'call_log_created'
-    routing_key = 'call_log.created'
+    routing_key_fmt = 'call_log.created'
 
-    def __init__(self, payload):
-        self.required_acl = 'events.{}'.format(self.routing_key)
-        self.payload = payload
-
-    def marshal(self):
-        return self.payload
-
-    def __eq__(self, other):
-        return self.payload == other.payload
-
-    def __ne__(self, other):
-        return not self == other
+    def __init__(self, cdr_data, tenant_uuid):
+        super(CallLogCreatedEvent, self).__init__(cdr_data, tenant_uuid)
 
 
-class CallLogUserCreatedEvent(object):
-
+class CallLogUserCreatedEvent(UserEvent):
     name = 'call_log_user_created'
-    routing_key_fmt = 'call_log.user.{}.created'
+    routing_key_fmt = 'call_log.user.{user_uuid}.created'
 
-    def __init__(self, user_uuid, payload):
-        self.routing_key = self.routing_key_fmt.format(user_uuid)
-        self.required_acl = 'events.{}'.format(self.routing_key)
-        self.payload = payload
-
-    def marshal(self):
-        return self.payload
-
-    def __eq__(self, other):
-        return self.payload == other.payload
-
-    def __ne__(self, other):
-        return not self == other
+    def __init__(self, cdr_data, tenant_uuid, user_uuid):
+        super(CallLogUserCreatedEvent, self).__init__(
+            cdr_data, tenant_uuid, user_uuid
+        )
