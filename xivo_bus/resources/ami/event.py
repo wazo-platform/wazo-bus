f458fea445f52e09dc466187123b3507ce08943c
Julien Alie <jalie@wazo.io>
2022-08-17 15:03:23 -0400

    update `wazo-amid` events

--- xivo_bus/resources/ami/event.py
+++ xivo_bus/resources/ami/event.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 Avencall
+# Copyright 2013-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import unicode_literals
+from xivo_bus.resources.common.event import ServiceEvent
 
 
-class AMIEvent(object):
+class AMIEvent(ServiceEvent):
+    name = '{ami_event}'
+    routing_key_fmt = 'ami.{name}'
 
-    def __init__(self, name, variables):
-        self.name = name
-        self.variables = variables
-        self.routing_key = 'ami.{}'.format(self.name)
-
-    def marshal(self):
-        return self.variables
+    def __init__(self, ami_event, variables):
+        self.name = type(self).name.format(ami_event=ami_event)
+        super(AMIEvent, self).__init__(variables)
