e7a30e08562b55b7030c19c93c4b2bbd51af550d
Julien Alie <jalie@wazo.io>
2022-08-22 13:56:43 -0400

    modified schema creation to avoid unnamed payloads

--- xivo_bus/documentation.py
+++ xivo_bus/documentation.py
@@ -1,18 +1,21 @@
+# Copyright 2022-2022 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
 import importlib
 import inspect
 import os
 import re
 import yaml
 
-from glob import glob
-from concurrent.futures import ThreadPoolExecutor
 from collections import ChainMap
+from concurrent.futures import ThreadPoolExecutor
+from glob import glob
+from threading import Lock
 from xivo_bus.resources.common.abstract import AbstractEvent
 
-# from xivo_bus.resources.common.event import TenantEvent, UserEvent
 
-
-RESOURCES_DIR = os.path.join("resources", "**", "event*.pyi")
+RESOURCES_DIR = os.path.join("resources", "**", "*.py")
 KEYS_IGNORE_LIST = (
     'self',
     'tenant_uuid',
@@ -20,59 +23,30 @@ KEYS_IGNORE_LIST = (
 )
 
 
-def generate_format():
-    spec = {
-        'asyncapi': '2.0.0',
-        'info': {
-            'title': '',
-            'version': '0.0.1',
-        },
-        'servers': {
-            'production': {
-                'url': 'amqp://guest:guest@localhost:5672/',
-                'protocol': 'amqp',
-                'protocolVersion': '0.9.1',
-            }
-        },
-        'channels': {},
-        'components': {
-            'x-headers': {
-                'origin_uuid': {
-                    'type': 'string',
-                    'format': 'uuid',
-                    'description': 'Unique identifier of the WAZO service that published this '
-                    'event',
-                },
-                'timestamp': {
-                    'type': 'string',
-                    'format': 'date-time',
-                    'description': 'date and time this event was published',
-                },
-                'tenant_uuid': {
-                    'type': 'string',
-                    'format': 'uuid',
-                    'description': 'Resource\'s associated tenant\'s UUID',
-                },
-                'user_uuid:{uuid}': {
-                    'name': 'user_uuid',
-                    'type': 'boolean',
-                    'description': 'Targeted user\'s uuid.  The format user_uuid:<uuid> allows '
-                    'multiple users to be the recipient of the event',
-                    'parameters': {
-                        'uuid': {
-                            'description': 'Concerned user\'s unique identifier',
-                            'schema': {
-                                'type': 'string',
-                                'format': 'uuid',
-                            },
-                        },
-                    },
-                },
-            },
-            'schemas': {},
-        },
-    }
-    return spec
+class SpecBuilder:
+    def __init__(self, schema_filename: str):
+        self._lock = Lock()
+        with open(schema_filename, 'r') as file:
+            self.schema: dict = yaml.safe_load(file)
+
+    def write(self, spec):
+        with self._lock:
+            self.schema['channels'].update(spec)
+
+    def render(self, filename=None):
+        if filename:
+            try:
+                with open(filename, 'w') as file:
+                    yaml.dump(self.schema, file, sort_keys=False)
+                return
+            except Exception:
+                raise
+        print(json.dumps(self.schema, indent=4))
+
+
+def camel_to_snake(str):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
 def generate_event_headers(cls, keys):
@@ -88,20 +62,20 @@ def generate_event_headers(cls, keys):
             'const': f'event.{cls.name}',
             'description': 'Necessary user access required to read this event',
         },
-        'origin_uuid': {'$ref': '#/components/x-headers/origin_uuid'},
-        'timestamp': {'$ref': '#/components/x-headers/timestamp'},
+        'origin_uuid': {'$ref': '#/components/schemas/origin_uuid'},
+        'timestamp': {'$ref': '#/components/schemas/timestamp'},
     }
 
     if 'tenant_uuid' in keys:
-        headers['tenant_uuid'] = {'$ref': '#/components/x-headers/tenant_uuid'}
+        headers['tenant_uuid'] = {'$ref': '#/components/schemas/tenant_uuid'}
         required.append('tenant_uuid')
     if 'user_uuid' in keys:
-        headers['user_uuid:{uuid}'] = {'$ref': '#/components/x-headers/user_uuid:{uuid}'}
+        headers['user_uuid:{uuid}'] = {'$ref': '#/components/schemas/user_uuid:{uuid}'}
         required.append('user_uuid:{uuid}')
     return headers, required
 
 
-def generate_event_payload(cls, keys):
+def generate_message(cls, keys):
     def contains(key, *parts):
         return any(part in key for part in parts)
 
@@ -115,70 +89,72 @@ def generate_event_payload(cls, keys):
             content[key] = {'type': 'integer'}
         elif key.endswith('_at'):
             content[key] = {'type': 'string', 'format': 'date-time'}
+        elif key.endswith(('_data', '_info', '_schema')):
+            content[key] = {'type': 'object'}
         else:
             content[key] = {'type': 'string'}
 
+    name = '-'.join([*camel_to_snake(cls.name).split('_'), 'payload'])
+    headers, required_headers = generate_event_headers(cls, keys)
+
+    payload = {'type': 'null'}
+    if content:
+        payload['type'] = 'object'
+        payload['properties'] = content
+
     return {
-        'data': {
+        'name': name,
+        'payload': payload,
+        'headers': {
             'type': 'object',
-            'properties': content,
+            'properties': headers,
+            'required': required_headers,
         },
     }
 
 
-def generate_event_parameters(cls):
-    parameters = {}
-    matches = re.search(r'\{(.*?)\}', cls.name)
-    if matches is None:
-        return {}
+def generate_base(cls, message):
+    try:
+        doc = yaml.safe_load(cls.__doc__)
+    except AttributeError:
+        doc = ''
 
-    for param in matches.groups() or []:
-        parameters[param] = {
-            'description': '',
-            'schema': {
-                'type': 'string',
-            },
+    base = {
+        'subscribe': {
+            'summary': doc or '',
+            'message': message,
         }
-    return {'parameters': parameters}
+    }
+
+    matches = re.search(r'\{(.*?)\}', cls.name)
+
+    if matches:
+        parameters = {}
+        for param in matches.groups() or []:
+            parameters[param] = {
+                'description': '',
+                'schema': {
+                    'type': 'string',
+                },
+            }
+        base['parameters'] = parameters
+
+    return {cls.name: base}
 
 
 def generate_event_spec(cls):
     fn_sig = inspect.signature(cls.__init__)
     fn_args = fn_sig.parameters.keys()
 
-    try:
-        doc = yaml.safe_load(cls.__doc__)
-    except AttributeError:
-        doc = {}
+    message = generate_message(cls, fn_args)
+    event_spec = generate_base(cls, message)
 
-    headers, required_headers = generate_event_headers(cls, fn_args)
-    payload = generate_event_payload(cls, fn_args)
-
-    event_spec = {
-        'subscribe': {
-            'summary': doc.get('summary', ''),
-            'message': {
-                'payload': {
-                    'type': 'object',
-                    'properties': payload,
-                },
-                'headers': {
-                    'type': 'object',
-                    'properties': headers,
-                    'required': required_headers,
-                },
-            },
-        }
-    }
-
-    parameters = generate_event_parameters(cls)
-    parameters.update(event_spec)
-    return parameters
+    return event_spec
 
 
-def get_event_spec(path):
+def get_module_event_specs(path):
     module_name = os.path.splitext(path)[0].replace('/', '.')
-    spec = {}
+    specs = {}
 
     try:
         module = importlib.import_module(module_name)
@@ -196,23 +172,19 @@ def get_event_spec(path):
         if not issubclass(cls, AbstractEvent):
             continue
 
-        spec[cls.name] = generate_event_spec(cls)
-    return spec
+        specs.update(generate_event_spec(cls))
+    return specs
 
 
 def import_events(base_cls=None):
     resources_dir = os.path.join("resources", "**", "event*.py")
 
+    builder = SpecBuilder('base_spec.yml')
     paths = sorted(glob(resources_dir, recursive=False), reverse=True)
-    spec = generate_format()
 
     with ThreadPoolExecutor() as executor:
-        results = executor.map(get_event_spec, paths)
+        results = executor.map(get_module_event_specs, paths)
 
-    spec['channels'] = dict(ChainMap(*[result for result in results]))
+    builder.write(dict(ChainMap(*[result for result in results])))
 
-    try:
-        with open('events_spec.yml', 'w') as file:
-            yaml.dump(spec, file, sort_keys=False)
-    except Exception:
-        raise
+    builder.render(filename='asyncapi.yml')
