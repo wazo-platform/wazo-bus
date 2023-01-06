1152c4b51a35da4a78e022d156b36b49f213b09a
Julien Alie <jalie@wazo.io>
2023-01-06 10:04:39 -0500

    remove deprecated ArbitraryEvent
    Why:
    
    * Dead code

--- xivo_bus/resources/common/event.py
+++ xivo_bus/resources/common/event.py
@@ -134,25 +134,3 @@ class ResourceConfigEvent:
 
     def __ne__(self, other):
         return self.id != other.id
-
-
-# Deprecated and should not be used for new events
-class ArbitraryEvent:
-    def __init__(self, name, body, required_acl=None):
-        self.name = name
-        self._body = dict(body)
-        if required_acl:
-            self.required_acl = required_acl
-
-    def marshal(self):
-        return self._body
-
-    def __eq__(self, other):
-        return (
-            self.name == other.name
-            and self._body == other._body
-            and self.required_acl == other.required_acl
-        )
-
-    def __ne__(self, other):
-        return self != other
