6dda3ebc080f9da3286ad791fac6d3d507c27c40
Julien Alie <jalie@wazo.io>
2024-01-03 10:01:28 -0500

    fix missing dependencies for tox (typing_extensions)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2013-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2013-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from setuptools import find_packages, setup
@@ -8,6 +8,7 @@ setup(
     name='xivo-bus',
     version='0.1',
     description='XiVO BUS libraries',
+    install_requires=["kombu==5.0.2", "typing_extensions==4.4.0"],
     author='Wazo Authors',
     author_email='dev@wazo.community',
     url='http://wazo.community',
