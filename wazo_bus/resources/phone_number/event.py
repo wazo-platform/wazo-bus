a915724680b8bd03143589e5409bfce47eac6221
Charles Langlois <charles.langlois@wazo.io>
2024-11-04 17:23:31 -0500

    resources.phone_number: added  PhoneNumberMainUpdatedEvent

--- wazo_bus/resources/phone_number/event.py
+++ wazo_bus/resources/phone_number/event.py
@@ -1,5 +1,6 @@
 # Copyright 2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
 
 from ..common.event import TenantEvent
 from ..common.types import UUIDStr
@@ -31,3 +32,20 @@ class PhoneNumberEditedEvent(TenantEvent):
 
     def __init__(self, phone_number: PhoneNumberDict, tenant_uuid: UUIDStr):
         super().__init__(phone_number, tenant_uuid)
+
+
+class PhoneNumberMainUpdatedEvent(TenantEvent):
+    service = 'confd'
+    name = 'phone_number_main_updated'
+    routing_key_fmt = 'config.phone_number.main.updated'
+
+    def __init__(
+        self,
+        current_main_uuid: str | None,
+        new_main_uuid: str | None,
+        tenant_uuid: UUIDStr,
+    ):
+        super().__init__(
+            {'current_main_uuid': current_main_uuid, 'new_main_uuid': new_main_uuid},
+            tenant_uuid,
+        )
