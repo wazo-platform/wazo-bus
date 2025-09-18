d30a945c13903872564792d4a1af91bd8de00713
Charles Langlois <charles.langlois@wazo.io>
2025-09-18 10:37:22 -0400

    requirements: clean & align
    no need for typing_extensions

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2013-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2013-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from setuptools import find_packages, setup
@@ -8,7 +8,7 @@ setup(
     name='wazo-bus',
     version='0.2',
     description='Wazo bus library',
-    install_requires=["kombu==5.0.2", "typing_extensions==4.4.0"],
+    install_requires=["kombu==5.2.4"],
     author='Wazo Authors',
     author_email='dev@wazo.community',
     url='http://wazo.community',
